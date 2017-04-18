569b0fafd5973808e06bc0950bf7013918d93043
Aleksandr Drozd <alex@smg.is.titech.ac.jp>
2017-04-18 17:18:33 +0900

    cleanup sysinfo

--- sysinfo/_sysinfo.py
+++ sysinfo/_sysinfo.py
@@ -6,33 +6,26 @@ import json
 
 
 def get_sys_info():
-	#result ="getting system info"
-	result = {}
-	result["linux"] = platform.platform()
-	result["linux"] = platform.platform()
-	#get GPU, 
+    result = {}
+    result["linux"] = platform.platform()
 
-		#GPU mem, 
-	#get CPU
-	cpu = cpuinfo.get_cpu_info()
-	result["cpu_brand"] = cpu["brand"]
-	result["cpu_count"] = cpu["count"]
-	result["hostname"] = platform.node()
-	result["hostname_short"] = platform.node().split(".")[0]
+    cpu = cpuinfo.get_cpu_info()
+    result["cpu_brand"] = cpu["brand"]
+    result["cpu_count"] = cpu["count"]
+    result["hostname"] = platform.node()
+    result["hostname_short"] = platform.node().split(".")[0]
 
-	try:
-		import pycuda.driver as cuda
-		import pycuda.autoinit
-		result["gpu"] = cuda.Device(0).name()
-	except:
-		result["gpu"] = "not detected"
+    try:
+        import pycuda.driver as cuda
+        import pycuda.autoinit
+        result["gpu"] = cuda.Device(0).name()
+#       gpu memory etc
+    except:
+        result["gpu"] = "not detected"
 
-	#get linux version
-	#get kernel version
+    return result
 
-	return result
 
-if 	__name__=="__main__":
-	info = get_sys_info()
-	print (json.dumps(info, sort_keys=True,  indent=4, separators=(',', ': ')))
-	#print (get_sys_info())
+if __name__ == "__main__":
+    info = get_sys_info()
+    print(json.dumps(info, sort_keys=True, indent=4, separators=(',', ': ')))
