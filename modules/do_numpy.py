ad7d095d0c5d7e3aa0d065841c695de4b4f0f35d
Aleksandr Drozd <alex@smg.is.titech.ac.jp>
2017-04-19 11:32:25 +0900

    exit if trying to use gpu with numpy

--- modules/do_numpy.py
+++ modules/do_numpy.py
@@ -5,6 +5,10 @@ from timeit import default_timer as timer
 def run(params):
     if params["problem"] != "2048": # todo change to actual parameter
         raise Exception("only 2048 problem is defined for numpy")
+    if "nb_gpus" in params:
+        if params["nb_gpus"]>0:
+            raise Exception("Numpy framework does not work with GPU back-end")
+
     a = np.random.random((2048, 2048))
     b = np.random.random((2048, 2048))
     nb_epoch = 1
