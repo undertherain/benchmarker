824c954e59cec349551fc2d527e8773d703f93c5
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-05-10 18:02:35 +0900

    matrix size as param

--- modules/do_numpy.py
+++ modules/do_numpy.py
@@ -2,15 +2,22 @@ import numpy as np
 from timeit import default_timer as timer
 
 
+def register_option():
+    
+
+
 def run(params):
     if params["problem"] != "2048": # todo change to actual parameter
         raise Exception("only 2048 problem is defined for numpy")
     if "nb_gpus" in params:
         if params["nb_gpus"]>0:
             raise Exception("Numpy framework does not work with GPU back-end")
+    
+    size = 2048
 
-    a = np.random.random((2048, 2048))
-    b = np.random.random((2048, 2048))
+    a = np.random.random((size, size))
+    b = np.random.random((size, size))
+    
     nb_epoch = 1
 
     start = timer()
