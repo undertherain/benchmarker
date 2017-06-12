3ee562d4f3073afd4dbaae7299d35012f2d81341
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-06-12 18:50:45 +0900

    helpers for viz moved to modules, viz for scaling

---
+++ vis/helpers.py
@@ -0,0 +1,19 @@
+import os
+import json
+import sys
+import pandas
+sys.path.append("./../sysinfo")
+from cute_device import get_cute_device_str
+
+
+def read_file(filename):
+    with open(filename) as f:
+        data = json.load(f)
+    return data
+
+
+def read_df_from_dir(path):
+    data = [read_file(os.path.join(path, f)) for f in os.listdir(path) if not f.startswith("arch") and os.path.isfile(os.path.join(path, f))]
+    df = pandas.DataFrame(data)
+    df["device"] = df["device"].apply(get_cute_device_str)
+    return df
