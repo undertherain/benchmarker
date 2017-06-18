9bbb70c32adcc4582c4e5931ff1067b11018ab5c
Kevin Brown <kevin.brown.jm@gmail.com>
2017-06-19 01:58:28 +0900

    Add ZMQ bandwidth(throughput) benchmark.

--- modules/do_zmq.py
+++ modules/do_zmq.py
@@ -1,26 +1,82 @@
-import cool_processor
+import os
+import sys
+import importlib
 
+class ZMQ(object):
+    def __init__(self, flags):
+        self.run_args      = ""
+        self.message_size  = "1"
+        self.iterations    = "1000"
+        self.interface     = "ib0"
+        self.server_port   = "5555"
 
+        self.__add_run_args(flags)
 
+    def __add_run_args(self, flags):
+        if "svr_ip" in flags:
+            self.server = flags["svr_ip"]
+        if "clt_ip" in flags:
+            self.client = flags["clt_ip"]
+
+        if "port" in flags:
+            self.server_port = flags["port"]
+        if "message_size" in flags:
+            self.message_size = flags["message_size"]
+        if "iterations" in flags:
+            self.iterations = flags["iterations"]
+        if "interface" in flags:
+            self.interface = flags["interface"]
+
+        self.run_args = [self.message_size, self.iterations]
+
+    def get_server_args(self):
+        binding = "tcp://" + self.interface + ":" + self.server_port
+        command = [binding]
+        command.extend(self.run_args)
+
+        return command
+
+    def get_client_args(self):
+        binding = "tcp://" + self.server + ":" + self.server_port
+        command = [binding]
+        command.extend(self.run_args)
+
+        return command
+
+    @staticmethod
+    def show_help():
+        print("Printing ZMQ help....\n")
 
 def run(params):
-	params["problem"] = "bandwidth"
+    params["problem"] = "bandwidth"
 
-	# Setup problem
-	mpi = OpenMPI()
+    # Prepare paramters
+    if params["misc"] == "help":
+        ZMQ.show_help()
+        exit(0)
 
-	mod       = importlib.import_module("problems." + params["problem"]+".zmq")
-	get_model = getattr(mod, 'get_model')
-	app       = get_model(params)
+    if params["misc"] == None:
+        print("There are no ZMQ flags provided.\nPlease use --misc=<flags>.")
+        ZMQ.show_help()
+        exit(1)
 
-	# setup runner: command/script, hostfile
-	output = app.execute(mpi.mpirun, mpi.get_args())
+    flags = params["misc"].split(",")
+    flags = dict(flag.split(':') for flag in flags)        
 
-	params["time"]        = output["timing"]
-	params["app_config"]  = output["config"]
+    # Setup benchmark
+    zmq = ZMQ(flags)
 
-	# Run ompi_info to get the version
-	ompi_version = ""
+    mod       = importlib.import_module("problems." + params["problem"]+".zmq")
+    get_model = getattr(mod, 'get_model')
+    app       = get_model(params)
 
-	params["framework_full"] = "Open MPI" + "_" + ompi_version
-	return params
+    # run actual benchmark and measure performance
+    output = app.execute(zmq.server, zmq.get_server_args(), zmq.client, zmq.get_client_args())
+    params["perf"]        = output["perf"]
+    params["app_config"]  = output["config"]
+
+    # Find the version of ZMQ
+    zmq_version = ""
+
+    params["framework_full"] = "ZMQ" + "_" + zmq_version
+    return params
--- modules/problems/bandwidth/zmq.py
+++ modules/problems/bandwidth/zmq.py
@@ -1,83 +1,68 @@
+from util import abstractprocess
 import os
 import sys
-import subprocess
 
-class ZMQ_Througput(object):
+""" This throughput test is based on these directions:
+        http://zeromq.org/results:perf-howto
+"""
+
+class ZMQ_Throughput():
     def __init__(self, params):
-		self.path          = os.path.dirname(os.path.realpath(__file__))
-		self.client_binary = self.path + "/" + "local_thr"
-		self.server_binary = self.path + "/" + "remote_thr"
-		self.run_args      = ""
-		self.port          = "5555"
-		self.message_size  = "1"
-		self.iterations    = "1000"
-		self.interface     = "eth0"
-
-		self.__add_run_args(params)
-		self.__add_hosts(params)
-        self.__compile(params)
+        self.path          = os.path.dirname(os.path.realpath(__file__))
+        self.client_binary = "remote_thr"
+        self.server_binary = "local_thr"
 
     def run_command(self, host_type):
-    	command = ""
-    	if host_type == "server":
-    		command = self.server_binary + " tcp://" + self.interface + ":"
-    	if host_type == "client":
-    		command = self.client_binary + " tcp://" + self.hosts[0] + ":"
+        command = []
+        if host_type == "server":
+            bin_path = os.path.join(self.path, self.server_binary)
+            command = [bin_path] + self.server_args
+        if host_type == "client":
+            bin_path = os.path.join(self.path, self.client_binary)
+            command = [bin_path] + self.client_args
 
-    	return command
+        return command
 
-    def execute(self):
-        server_cmd  = self.run_command("server")
-        client_cmd  = self.run_command("client")
+    def execute(self, server, server_args, client, client_args):
+        self.server_args   = server_args
+        self.client_args   = client_args
 
-        server_proc = subprocess.Popen(server_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        client_proc = subprocess.Popen(client_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = server_proc.communicate()
+        server_command  = self.run_command("server")
+        client_command  = self.run_command("client")
 
-        if len(stderr) == 0:
-            val = {} 
-            val["timing"] = self.__parse_output(stdout.splitlines()[])
-            val["config"] = {"server_command": server_cmd, "client_command": client_cmd}
-        else:
-            print("Server command: " + server_cmd)
-            print("Client command: " + client_cmd)
+        server_proc = abstractprocess.Process("ssh", host=server, command=server_command)
+        client_proc = abstractprocess.Process("ssh", host=client, command=client_command)
+        output = server_proc.get_output()
+
+        if output["returncode"] != 0:
+            print("Server command: " + " ".join(server_command))
+            print("Client command: " + " ".join(client_command))
             print("ERROR:")
-            print(stderr)
+            print(output)
             exit(1)
+
+        val = {"perf": {}} 
+        val["perf"]["throughput"], val["perf"]["unit"] = self.__parse_output(output["out"].splitlines()[3])
+        
+        val["config"] = { "server": server, "server_command": " ".join(server_command), 
+                          "client": client, "client_command": " ".join(client_command)}
 
         return val
 
-    def __compile(self, params=None):
-        pass
-
-    def __add_hosts(self, params):
-    	# TODO: add functionality for hostfile
-    	self.hosts = params["hosts"].split(",")
-
-	def __add_run_args(self, params):
-		if "port" in params:
-			self.port = params["port"]
-		if "message_size" in params:
-		    self.message_size = params["message_size"]
-		if "iterations" in params:
-		    self.iterations = params["iterations"]
-		if "interface" in params:
-			self.interface = params["interface"]
-
-		self.run_args = " ".join([self.port, self.message_size, self.iterations])
-
     def __parse_output(self, output):
-    	import re
+        import re
 
-    	regex = re.compile()
-        results = {}
-        for line in output:
-            size, t_avg, t_min, t_max, iterations = line.split()
-            size = int(size)
-            results[size] = {}
-            results[size]["avg"] = float(t_avg)
-            results[size]["min"] = float(t_min)
-            results[size]["max"] = float(t_max)
-            results[size]["iterations"] = int(iterations)
+        # Format: 'mean throughput: 37.736 [Mb/s]'
+        regex = re.compile("mean throughput:\s+([\d|\.]+)\s+\[(.+)\]")
 
-        return results
+        if not regex.match(output):
+            print("Unexpected output:")
+            print(output)
+            exit(1)
+
+        return (regex.match(output).group(1), regex.match(output).group(2))
+
+def get_model(params):
+    app = ZMQ_Throughput(params)
+
+    return app
