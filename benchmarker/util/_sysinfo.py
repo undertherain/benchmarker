a900c567d06384e0bf654d6cebd3d1438b05aa98
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-10-27 12:42:57 +0900

    rely on system package systemquery

--- benchmarker/util/_sysinfo.py
+++ benchmarker/util/_sysinfo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import json
-from system_query import system_query
+from system_query import query_all
 
 if __name__ == "__main__":
-    info = system_query.query_all()
+    info = query_all()
     print(json.dumps(info, sort_keys=True, indent=4, separators=(',', ': ')))
