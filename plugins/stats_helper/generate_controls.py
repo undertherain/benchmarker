e7dfbcd412b32e60033d8b87538cf3c039aca0f8
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-07-18 23:24:21 +1000

    shortcode plugins to generate controls

---
+++ plugins/stats_helper/generate_controls.py
@@ -0,0 +1,11 @@
+from nikola.plugin_categories import ShortcodePlugin
+
+class Plugin(ShortcodePlugin):
+
+    name = "generate_controls"
+
+    def handler(self, filename=None, site=None, data=None, lang=None, post=None):
+        """Create HTML for emoji."""
+        output = "Hi I'm plugin"
+
+        return output, []
