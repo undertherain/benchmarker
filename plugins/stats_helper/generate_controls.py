58571d4d29650182d01fe3565283640dd4f62627
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-07-23 22:02:58 +0900

    typos

--- plugins/stats_helper/generate_controls.py
+++ plugins/stats_helper/generate_controls.py
@@ -1,9 +1,29 @@
+import json
 import os
+import pandas
 from nikola.plugin_categories import ShortcodePlugin
 from mako.template import Template
 
 
 plugin_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def read_file(filename):
+    with open(filename) as f:
+        data = json.load(f)
+    return data
+
+
+def read_df_from_dir(path):
+    data = [read_file(os.path.join(path, f)) for f in os.listdir(path) if not f.startswith("arch") and os.path.isfile(os.path.join(path, f))]
+    df = pandas.DataFrame(data)
+    # df["device"] = df["device"].apply(get_cute_device_str)
+    return df
+
+
+def read_jsons():
+    df = read_df_from_dir(os.path.join(plugin_path, "../../data"))
+    print(df)
 
 
 class Plugin(ShortcodePlugin):
@@ -17,10 +37,16 @@ class Plugin(ShortcodePlugin):
         keyvals = {
             "kernel": ["conv2", "conv3"],
             "device": ["p100", "v100"],
-            "framework": ["Chainer", "TensorFlow", "MxNET"]
+            "framework": ["Chainer", "TensorFlow", "MXNet"]
             }
         output = mytemplate.render(keyvals=keyvals)
-        #output += mytemplate.render(key="device", vals=["p100", "v100"])
-        #output += mytemplate.render(key="framework", vals=["Chainer", "TensorFlow"])
 
         return output, []
+
+
+def main():
+    read_jsons()
+
+
+if __name__ == "__main__":
+    main()
