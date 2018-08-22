efe80bb59d9b6b0b6bf4e8a2221868340e2e4dc2
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-08-22 13:20:59 +0900

    size option for VGG

--- benchmarker/modules/problems/vgg16/data.py
+++ benchmarker/modules/problems/vgg16/data.py
@@ -1,9 +1,13 @@
+""""sinthetic dataset for vgg16"""
+
 from benchmarker.util.data.cubes import get_cubes
 
-#def get_data(params):
-#    X, Y = get_cubes(dims=2, edge=224, channels=3, cnt=10*1024, channels_first=params["channels_first"])
-#    Y = to_one_hot(Y, cnt_classes=1000)
-#    return X, Y
 
 def get_data(params):
-    return get_cubes(dims=2, edge=224, channels=3, cnt_samples=1024, channels_first=params["channels_first"], onehot=False)
+    """generates sinthetic dataset"""
+    if "size" in params["problem"]:
+        cnt_samples = params["problem"]["size"]
+    else:
+        cnt_samples = 1024
+    return get_cubes(dims=2, edge=224, channels=3, cnt_samples=cnt_samples,
+                     channels_first=params["channels_first"], onehot=False)
