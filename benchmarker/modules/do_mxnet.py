1b92a18386dbf1594dfeb524a6cb19c8b8848e33
Aleksandr Drozd <alexander.drozd@gmail.com>
2019-03-27 16:03:24 +0900

    update mxnet backend

--- benchmarker/modules/do_mxnet.py
+++ benchmarker/modules/do_mxnet.py
@@ -19,35 +19,48 @@ class DoMxnet(INeuralNet):
         # self.params["channels_first"] = True
 
     def run(self):
+        params = self.params
         x_train, y_train = self.load_data()
 
         mod = importlib.import_module("benchmarker.modules.problems." +
-                                      self.params["problem"]["name"] + ".mxnet")
-        get_model = getattr(mod, 'get_model')
-
-        net = get_model(x_train[0].shape)
-        if self.params["nb_gpus"] > 0:
-            devices = [mx.gpu(i) for i in self.params["gpus"]]
-        else:
-            devices = mx.cpu()
-        if self.params["nb_gpus"] == 1:
-            devices = mx.gpu(self.params["gpus"][0])
-
-        print(devices)
-        nb_epoch = 3
+                                      params["problem"]["name"] + ".mxnet")
+        net = getattr(mod, 'Net')
+        data = mx.sym.var('data')
+        # if opt.dtype == 'float16':
+        #     data = mx.sym.Cast(data=data, dtype=np.float16)
+        out = net(data)
+        # if opt.dtype == 'float16':
+        #     out = mx.sym.Cast(data=out, dtype=np.float32)
+        softmax = mx.sym.SoftmaxOutput(out, name='softmax')
+        mod = mx.mod.Module(softmax, context=mx.cpu())
+#         train_data, val_data = get_data_iters(dataset, batch_size, opt)
         train_iter = mx.io.NDArrayIter(x_train, y_train, batch_size=self.params["batch_size"])
-        mod = mx.mod.Module(symbol=net,
-                            # context=mx.cpu(),
-                            context=devices,
-                            data_names=['data'],
-                            label_names=['softmax_label'])
+
         print("preheat")
         mod.fit(train_iter,
-                # eval_data=data.get_iter(batch_size),
+                # eval_data=val_data,
+                num_epoch=1,
+                # kvstore=kv,
+                # batch_end_callback=mx.callback.Speedometer(params[]batch_size, max(1, opt.log_interval)),
+                # epoch_end_callback=mx.callback.do_checkpoint('image-classifier-%s'% opt.model),
                 optimizer='sgd',
                 optimizer_params={'learning_rate': 0.1},
-                eval_metric='acc',
-                num_epoch=1)
+                # optimizer_params={'learning_rate': opt.lr, 'wd': opt.wd, 'momentum': opt.momentum, 'multi_precision': True},
+                initializer=mx.init.Xavier(magnitude=2))
+
+# if self.params["nb_gpus"] > 0:
+        #     devices = [mx.gpu(i) for i in self.params["gpus"]]
+        # else:
+        #     devices = mx.cpu()
+        # if self.params["nb_gpus"] == 1:
+        #     devices = mx.gpu(self.params["gpus"][0])
+
+        # print(devices)
+#        mod = mx.mod.Module(symbol=net,
+#                            # context=mx.cpu(),
+#                            context=devices,
+#                            data_names=['data'],
+#                            label_names=['softmax_label'])
         print("train")
         start = timer()
         mod.fit(train_iter,
@@ -55,9 +68,10 @@ class DoMxnet(INeuralNet):
                 optimizer='sgd',
                 optimizer_params={'learning_rate': 0.1},
                 eval_metric='acc',
-                num_epoch=nb_epoch)
+                num_epoch=params["nb_epoch"])
         end = timer()
-        self.params["time"] = (end-start) / nb_epoch
+        self.params["time"] = (end - start) / params["nb_epoch"]
+        self.params["time_epoch"] = (end - start) / params["nb_epoch"]
         self.params["framework_full"] = "MXNet-" + mx.__version__
         return self.params
 
--- benchmarker/modules/problems/resnet50/mxnet.py
+++ benchmarker/modules/problems/resnet50/mxnet.py
@@ -1,3 +1,4 @@
 from mxnet.gluon.model_zoo import vision
 
-Net = vision.resnet50_v1(pretrained=False)
+#Net = vision.resnet50_v1(pretrained=False)
+Net = vision.get_model("resnet50_v1")
