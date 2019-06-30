4afdedf47ee5f335eb172dbdaa806cd4d78256a4
Aleksandr Drozd <alexander.drozd@gmail.com>
2019-06-30 15:09:05 +0900

    hard-coded res10-ssd

--- benchmarker/modules/do_opencv.py
+++ benchmarker/modules/do_opencv.py
@@ -1,24 +1,38 @@
 import cv2
+from timeit import default_timer as timer
+
 from .i_neural_net import INeuralNet
 
 
 class BackendOpenCV(INeuralNet):
+    # move to problem module
+    PATH_PROTO = "/mnt/kodi/blackbird/Scry/models/3rd_party/res10_ssd/deploy.prototxt.txt"
+    PATH_WEIGHTS = "/mnt/kodi/blackbird/Scry/models/3rd_party/res10_ssd/res10_300x300_ssd_iter_140000.caffemodel"
+
     def __init__(self, params):
         super().__init__(params)
-        self.params["channels_first"] = True
-        self.params["nb_epoch"] = 10
+        self.params["channels_first"] = False
+        self.params["nb_epoch"] = 1
 
     def run(self):
+        params = self.params
         x_train, y_train = self.load_data()
-        PATH_PROTO = "data/cv2/ssd/deploy.prototxt.txt"
-        PATH_WEIGHTS = "data/cv2/ssd/res10_300x300_ssd_iter_140000.caffemodel"
         self.net = cv2.dnn.readNetFromCaffe(self.PATH_PROTO, self.PATH_WEIGHTS)
-        blob = cv2.dnn.blobFromImage(frame,
-                                     scalefactor=1.0,
-                                     size=(300, 300),
-                                     mean=(104.0, 177.0, 123.0))
-        self.net.setInput(blob)
-        predictions = self.net.forward()
+        start = timer()
+        for i in range(x_train.shape[0]):
+            frame = x_train[0]
+            blob = cv2.dnn.blobFromImage(frame,
+                                         scalefactor=1.0,
+                                         size=(300, 300),
+                                         mean=(104.0, 177.0, 123.0))
+            self.net.setInput(blob)
+            predictions = self.net.forward()
+            assert predictions is not None
+        end = timer()
+        params["time"] = (end - start) / self.params["nb_epoch"]
+        params["time_epoch"] = (end - start) / self.params["nb_epoch"]
+        params["framework_full"] = "opencv-" + cv2.__version__
+        return self.params
 
 
 def run(params):
