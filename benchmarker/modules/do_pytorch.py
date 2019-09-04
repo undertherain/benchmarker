ff99248cbc04525359b53e43efbaf45d2c259c94
Aleksandr Drozd <alexander.drozd@gmail.com>
2019-09-04 14:23:56 +0900

    comment out loading test data

--- benchmarker/modules/do_pytorch.py
+++ benchmarker/modules/do_pytorch.py
@@ -26,23 +26,23 @@ class DoPytorch(INeuralNet):
                     epoch, batch_idx * len(data), len(train_loader.dataset),
                     100. * batch_idx / len(train_loader), loss.item()))
 
-    def test(args, model, device, test_loader):
-        model.eval()
-        test_loss = 0
-        correct = 0
-        with torch.no_grad():
-            for data, target in test_loader:
-                data, target = data.to(device), target.to(device)
-                output = model(data)
-                test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-                pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-                correct += pred.eq(target.view_as(pred)).sum().item()
+    # def test(args, model, device, test_loader):
+    #     model.eval()
+    #     test_loss = 0
+    #     correct = 0
+    #     with torch.no_grad():
+    #         for data, target in test_loader:
+    #             data, target = data.to(device), target.to(device)
+    #             output = model(data)
+    #             test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+    #             pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+    #             correct += pred.eq(target.view_as(pred)).sum().item()
 
-        test_loss /= len(test_loader.dataset)
+    #     test_loss /= len(test_loader.dataset)
 
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset),
-            100. * correct / len(test_loader.dataset)))
+    #     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    #         test_loss, correct, len(test_loader.dataset),
+    #         100. * correct / len(test_loader.dataset)))
 
     def run(self):
         # Training settings
@@ -83,12 +83,12 @@ class DoPytorch(INeuralNet):
                                transforms.Normalize((0.1307,), (0.3081,))
                            ])),
             batch_size=self.params["batch_size"], shuffle=True, **kwargs)
-        test_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.1307,), (0.3081,))
-                           ])),
-            batch_size=self.params["batch_size"], shuffle=True, **kwargs)
+        # test_loader = torch.utils.data.DataLoader(
+        #     datasets.MNIST('../data', train=False, transform=transforms.Compose([
+        #                        transforms.ToTensor(),
+        #                        transforms.Normalize((0.1307,), (0.3081,))
+        #                    ])),
+        #     batch_size=self.params["batch_size"], shuffle=True, **kwargs)
             # TODO: create arg for test barch size
 
         model = self.net().to(device)
