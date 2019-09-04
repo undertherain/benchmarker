bffa8965c67706d4ff73f18fe776e49df4be6d2c
Aleksandr Drozd <alexander.drozd@gmail.com>
2019-09-04 13:54:00 +0900

    refactoring to common api

--- benchmarker/modules/do_pytorch.py
+++ benchmarker/modules/do_pytorch.py
@@ -57,32 +57,34 @@ def test(args, model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
-def main():
+def run(params):
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                        help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                        help='learning rate (default: 0.01)')
-    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                        help='SGD momentum (default: 0.5)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
+    # parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    # parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    #                     help='input batch size for training (default: 64)')
+    # parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+    #                     help='input batch size for testing (default: 1000)')
+    # parser.add_argument('--epochs', type=int, default=10, metavar='N',
+    #                     help='number of epochs to train (default: 10)')
+    # parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+    #                     help='learning rate (default: 0.01)')
+    # parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+    #                     help='SGD momentum (default: 0.5)')
+    # parser.add_argument('--no-cuda', action='store_true', default=False,
+    #                     help='disables CUDA training')
+    # parser.add_argument('--seed', type=int, default=1, metavar='S',
+    #                     help='random seed (default: 1)')
+    # parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+    #                     help='how many batches to wait before logging training status')
     
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    # parser.add_argument('--save-model', action='store_true', default=False,
+    #                     help='For Saving the current Model')
+    # args = parser.parse_args()
 
-    torch.manual_seed(args.seed)
+    # use_cuda = not args.no_cuda and torch.cuda.is_available()
+    use_cuda = False
+
+    # torch.manual_seed(args.seed)
 
     device = torch.device("cuda" if use_cuda else "cpu")
 
@@ -93,7 +95,7 @@ def main():
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
+        batch_size=params["batch_size"], shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=False, transform=transforms.Compose([
                            transforms.ToTensor(),
@@ -109,8 +111,5 @@ def main():
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
 
-    if (args.save_model):
-        torch.save(model.state_dict(),"mnist_cnn.pt")
-        
-if __name__ == '__main__':
-    main()
+    # TODO: return stats
+    return {}
