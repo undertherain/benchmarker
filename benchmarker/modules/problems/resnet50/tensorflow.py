13e62124065f128256bec0fcc5be635b1bec2e7f
Aleksandr Drozd <alexander.drozd@gmail.com>
2019-09-11 21:37:50 +0900

    tf

--- benchmarker/modules/problems/resnet50/tensorflow.py
+++ benchmarker/modules/problems/resnet50/tensorflow.py
@@ -1,11 +1,14 @@
-import keras
-from keras.applications.resnet50 import ResNet50
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.applications.resnet50 import ResNet50
 
 
 def get_model(shape):
     model = ResNet50(weights=None)
-    optimizer = keras.optimizers.Adam()
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=["accuracy"])
+    optimizer = SGD()
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizer,
+                  metrics=["accuracy"])
     return model
+
 
 Net = get_model
