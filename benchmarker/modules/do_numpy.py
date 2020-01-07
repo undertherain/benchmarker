e1f6cdbade364381d4e762afd344709da38b511b
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-01-07 14:20:08 +0900

    move more logic to base

--- benchmarker/modules/do_numpy.py
+++ benchmarker/modules/do_numpy.py
@@ -14,34 +14,20 @@ class Benchmark(IGEMM):
 
     def run(self):
         params = self.params
-        if params["problem"]["name"] != "gemm":
-            raise Exception("only gemm problem is defined for numpy")
         if "nb_gpus" in params:
             if params["nb_gpus"] > 0:
                 raise Exception("Numpy framework does not work with GPU back-end")
-
-        if isinstance(params["problem"]["size"], int):
-            M = N = K = params["problem"]["size"]
-        else:
-            M, N, K = params["problem"]["size"]
+        M, N, K = self.matrix_size
         dtype = np.float32
         a = np.random.random((M, N)).astype(dtype)
         b = np.random.random((N, K)).astype(dtype)
         c = np.random.random((M, K)).astype(dtype)
-
         nb_epoch = 2
-
         time_start = timer()
         for _ in range(nb_epoch):
             c = a @ b # + c
         time_end = timer()
-        gflop = (2.0 * M * N * K) / (1024 ** 3)
-        #print(f"GFlop: {gflop:.5f}")
         elapsed_time = (time_end - time_start) / nb_epoch
-        #print(f"time: {elapsed_time:.5f}")
-        #print(f"GFlop/sec: {gflop / elapsed_time:.5f}")
-
         params["time"] = elapsed_time
-        params["GFLOP"] = gflop
-        params["GFLOP/sec"] = gflop / elapsed_time
+        params["GFLOP/sec"] = params["GFLOP"] / elapsed_time
         return params
--- benchmarker/modules/i_gemm.py
+++ benchmarker/modules/i_gemm.py
@@ -16,3 +16,13 @@ class IGEMM():
         params["problem"]["precision"] = "FP32"
         params["path_out"] = os.path.join(params["path_out"],
                                           params["problem"]["precision"])
+        if isinstance(params["problem"]["size"], int):
+            M = N = K = params["problem"]["size"]
+        else:
+            M, N, K = params["problem"]["size"]
+        self.matrix_size = M, N, K
+        params["problem"]["size"] = self.matrix_size
+        gflop = (2.0 * M * N * K) / (1024 ** 3)
+        params["GFLOP"] = gflop
+        if params["problem"]["name"] != "gemm":
+            raise Exception("only gemm problem is defined for this framework")
