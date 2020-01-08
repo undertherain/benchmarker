fef27ce0ad284d972e701d08de3d8b2f92f05a3e
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-01-08 12:32:17 +0900

    use json_normalize

--- benchmarker/vis/helpers.py
+++ benchmarker/vis/helpers.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 import json
 import pandas
+from pandas.io.json import json_normalize
 from benchmarker.util.cute_device import get_cute_device_str
 
 
 def read_file(filename):
     with open(filename) as f:
         data = json.load(f)
-    return data
+    return json_normalize(data)
 
 
 def read_df_from_dir(path):
@@ -15,7 +16,7 @@ def read_df_from_dir(path):
     data = [read_file(f) for f in path.glob("**/*.json") if not f.name.startswith("arch")]
     if len(data) == 0:
         raise RuntimeError("no data to load")
-    df = pandas.DataFrame(data)
+    df = pandas.concat(data)
     df["device"] = df["device"].apply(get_cute_device_str)
     return df
 
