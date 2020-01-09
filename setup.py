aa0bf257e72bfc9b0178c19b29d36adc59df679b
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-01-09 11:05:04 +0900

    add setup scripts

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""Setup script for benchmarker package."""
+
+import setup_boilerplate
+
+
+class Package(setup_boilerplate.Package):
+
+    """Package metadata."""
+
+    name = 'protonn'
+    description = 'performance benchamrking framework'
+    url = "https://benchmarker.blackbird.pw/"
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering']
+    keywords = ['performance', 'benchmarking', 'neural network']
+
+
+if __name__ == '__main__':
+    Package.setup()
