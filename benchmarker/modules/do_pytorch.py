bd75a6f7e3430f835a0a9e1476748fee151f6495
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-01-15 11:36:54 +0900

    gpu backend for pytorch

--- benchmarker/modules/do_pytorch.py
+++ benchmarker/modules/do_pytorch.py
@@ -54,33 +54,19 @@ class Benchmark(INeuralNet):
     def run_internal(self):
         # use_cuda = not args.no_cuda and torch.cuda.is_available()
         if self.params["gpus"]:
-            raise RuntimeError("GPU support is not implemented yet")
-        use_cuda = False
+            device = torch.device("cuda")
+        else:
+            device = torch.device("cpu")
 
+            # raise RuntimeError("GPU support is not implemented yet")
+            # use_cuda = False
         # torch.manual_seed(args.seed)
-
-        device = torch.device("cuda" if use_cuda else "cpu")
-
         # kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
         x_train, y_train = self.load_data()
         x_train = torch.from_numpy(x_train)
         y_train = torch.from_numpy(y_train.astype(np.int64))
         train_dataset = torch.utils.data.TensorDataset(x_train, y_train)
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=self.params["batch_size"], shuffle=True)
-        # train_loader = torch.utils.data.DataLoader(
-        #     datasets.MNIST('../data', train=True, download=True,
-        #                    transform=transforms.Compose([
-        #                        transforms.ToTensor(),
-        #                        transforms.Normalize((0.1307,), (0.3081,))
-        #                    ])),
-        #     batch_size=self.params["batch_size"], shuffle=True, **kwargs)
-        # test_loader = torch.utils.data.DataLoader(
-        #     datasets.MNIST('../data', train=False, transform=transforms.Compose([
-        #                        transforms.ToTensor(),
-        #                        transforms.Normalize((0.1307,), (0.3081,))
-        #                    ])),
-        #     batch_size=self.params["batch_size"], shuffle=True, **kwargs)
-            # TODO: create arg for test barch size
 
         model = self.net.to(device)
         # TODO: args for training hyperparameters
