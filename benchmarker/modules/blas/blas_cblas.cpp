c485b70ca742a0c20125ad65809398f9db09ca2b
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-01-15 10:38:14 +0900

    update clbasl

--- benchmarker/modules/blas/blas_cblas.cpp
+++ benchmarker/modules/blas/blas_cblas.cpp
@@ -9,13 +9,18 @@ int main() {
     double dtime;
     size_t i;
     // TODO: move this to shared module
+    printf("doing clbas");
     A = (t_float*) malloc(sizeof(t_float)*n*n);
     B = (t_float*) malloc(sizeof(t_float)*n*n);
     C = (t_float*) malloc(sizeof(t_float)*n*n);
     for(i=0; i<n*n; i++) { A[i] = rand()/RAND_MAX; B[i] = rand()/RAND_MAX;}
+    printf("done random init");
 
     dtime = omp_get_wtime();
     cblas_sgemm(CblasColMajor, CblasNoTrans, CblasTrans,n,n,n, 1, A, n, B, n, 1, C ,n);
     dtime = omp_get_wtime() - dtime;
-    printf("Openblas\t%f\n", dtime);
+    double gflop = (2.0 * n * n * n) / (1024 * 1024 * 1024);
+    double gflops = gflop / dtime;
+    printf("time: \t%f\n", dtime);
+    printf("flops/s: \t%f\n", gflops);
 }
