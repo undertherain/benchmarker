8ae17e71712bb4036ccd1f88db6013f53a4594bb
Aleksandr Drozd <alexander.drozd@gmail.com>
2020-01-21 13:22:32 +0900

    matrix size as args

--- benchmarker/modules/blas/blas_cblas.cpp
+++ benchmarker/modules/blas/blas_cblas.cpp
@@ -4,15 +4,29 @@
 #include <omp.h>
 #include "config.h"
 
-int main(int argc, char * argv[]) {    
+int main(int argc, char * argv[]) {
+    long m, n, k;
+    if (argc==2) {
+        m = atoi(argv[1]);
+        n = m;
+        k = m;
+    }
+    else
+        if (argc==4) {
+            m = atoi(argv[1]);
+            n = atoi(argv[2]);
+            k = atoi(argv[3]);
+        }
+        else
+            return -1;
     t_float *A, *B, *C;
     double dtime;
     size_t i;
     // TODO: move this to shared module
     fprintf(stderr, "doing clbas\n");
-    A = (t_float*) malloc(sizeof(t_float)*n*n);
-    B = (t_float*) malloc(sizeof(t_float)*n*n);
-    C = (t_float*) malloc(sizeof(t_float)*n*n);
+    A = (t_float*) malloc(sizeof(t_float)*m*n);
+    B = (t_float*) malloc(sizeof(t_float)*n*k);
+    C = (t_float*) malloc(sizeof(t_float)*m*n);
     for(i=0; i<n*n; i++) { A[i] = rand()/RAND_MAX; B[i] = rand()/RAND_MAX;}
     fprintf(stderr, "done random init\n");
 
--- benchmarker/modules/blas/config.h
+++ benchmarker/modules/blas/config.h
@@ -1,2 +1,1 @@
 typedef float t_float;
-size_t n=1024;
