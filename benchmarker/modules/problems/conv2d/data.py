cee325b082ab6b7f0d2da9958f88621e5f92f197
Sasha <alexander.drozd@gmail.com>
2020-03-02 21:46:52 +0900

    wip conv2d

---
+++ benchmarker/modules/problems/conv2d/data.py
@@ -0,0 +1,14 @@
+from benchmarker.util.data.cubes import get_cubes
+
+
+def get_data(params):
+    """generates sinthetic dataset"""
+
+    if "size" in params["problem"]:
+        cnt_samples = params["problem"]["size"]
+    else:
+        cnt_samples = 128
+
+    return get_cubes(dims=2, edge=224, channels=3,
+                     cnt_samples=cnt_samples,
+                     channels_first=params["channels_first"], onehot=False)
---
+++ benchmarker/modules/problems/conv2d/pytorch.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+# import torch.nn.functional as F
+
+# TODO: move this to params
+cnt_channels = 3
+size_image = 224
+cnt_filters = 3
+size_kernel = 3
+size_batch = 32
+cnt_repeats = 2
+
+Net = nn.Conv2d(in_channels=cnt_channels,
+                out_channels=cnt_filters,
+                kernel_size=size_kernel,
+                stride=1,
+                padding=1,
+                dilation=1,
+                groups=1,
+                bias=True,
+                padding_mode='zeros')
