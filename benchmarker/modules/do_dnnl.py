dbe897c544a982a1a641609268c5aaeeca09175e
Sasha <alexander.drozd@gmail.com>
2020-03-06 11:16:50 +0900

    dnnl

--- benchmarker/modules/do_dnnl.py
+++ benchmarker/modules/do_dnnl.py
@@ -1,11 +1,15 @@
+import os
 from benchmarker.util.abstractprocess import Process
 # form a command line from parameters
-# call DNNL bench
-command = ["./benchdnn",
+path_benchdnn = "/home/blackbird/Projects_heavy/DL/mkl-dnn/build/tests/benchdnn"
+spec_run = "mb64ic3ih224oc32oh224kh3ph1n\"myconv\""
+command = [os.path.join(path_benchdnn, "./benchdnn"),
 		   "--conv",
 		   "--mode=p",
-		   "mb64ic3ih224oc32oh224kh3ph1n\"myconv\""]
-process = Process(command=["ls"])
+		   spec_run]
+process = Process(command=command)
 result = process.get_output()
 std_out = result["out"]
+print(type(std_out))
+# parse out
 print(std_out)
