bd43285f4fd3ec8a2c117e47714a3bdcefc556ee
Sasha <alexander.drozd@gmail.com>
2020-03-11 15:11:10 +0900

    gpu sync in cupy backend

--- benchmarker/modules/do_cupy.py
+++ benchmarker/modules/do_cupy.py
@@ -17,13 +17,16 @@ class Benchmark(IGEMM):
                 raise Exception("1 GPU is needed for CuPy")
         M, N, K = self.matrix_size
         dtype = cupy.float32
+        device = cupy.cuda.Device(device=0)
+        device.use()
         a = cupy.random.random((M, N)).astype(dtype)
         b = cupy.random.random((N, K)).astype(dtype)
         c = cupy.random.random((M, K)).astype(dtype)
-        nb_epoch = 2
+        nb_epoch = 4
         time_start = timer()
         for _ in range(nb_epoch):
             c = a @ b # + c
+        device.synchronize()
         time_end = timer()
         elapsed_time = (time_end - time_start) / nb_epoch
         self.params["time"] = elapsed_time
