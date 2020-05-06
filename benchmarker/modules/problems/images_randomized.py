c5d5a61c8a70910ad39c4ea622ea9ab3d32faf33
Emil VATAI <emil.vatai@gmail.com>
2020-05-06 20:09:45 +0900

    Fix `gen_images()` in PR #8 commit 7def2ca
    The new code merged in PR #8 caused the (previously working) resnet50
    on pytorch benchmark to fail.  The reason was that it restricted
    `gen_images()` to work only on convXd problems and failed other
    problems such as resnet50.  The code was also simplified a bit.

--- benchmarker/modules/problems/images_randomized.py
+++ benchmarker/modules/problems/images_randomized.py
@@ -5,35 +5,19 @@ import numpy as np
 def gen_images(params):
     """Generates synthetic dataset."""
 
-    if (params["problem"]["name"]) == 'conv1d':
+    shape = (params["problem"]["cnt_batches_per_epoch"], params["batch_size"])
+    shape = shape + params["problem"]["size"][1:]
+
+    # TODO(vatai): Not sure if these asserts are neede.
+    if (params["problem"]["name"]) == "conv1d":
         # (cnt_batches, batch, channels, x)
         assert len(params["problem"]["size"]) == 3
-
-        shape = (params["problem"]["cnt_batches_per_epoch"],
-                 params["batch_size"],
-                 params["problem"]["size"][1],
-                 params["problem"]["size"][2])
-
-    elif (params["problem"]["name"]) == 'conv2d':
-      # (cnt_batches, batch, channels, x, h,w)
+    elif (params["problem"]["name"]) == "conv2d":
+        # (cnt_batches, batch, channels, x, h,w)
         assert len(params["problem"]["size"]) == 4
-
-        shape = (params["problem"]["cnt_batches_per_epoch"],
-                 params["batch_size"],
-                 params["problem"]["size"][1],
-                 params["problem"]["size"][2],
-                 params["problem"]["size"][3])
-
-    elif (params["problem"]["name"]) == 'conv3d':
+    elif (params["problem"]["name"]) == "conv3d":
         # (cnt_batches, batch, channels, x, h,w)
         assert len(params["problem"]["size"]) == 5
-
-        shape = (params["problem"]["cnt_batches_per_epoch"],
-                 params["batch_size"],
-                 params["problem"]["size"][1],
-                 params["problem"]["size"][2],
-                 params["problem"]["size"][3],
-                 params["problem"]["size"][4])
 
     X = np.random.random(shape).astype(np.float32)
     Y = np.random.random(shape[:2]).astype(np.int64)
