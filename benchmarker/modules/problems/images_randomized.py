775ac881916bf9a5983657867ef7dbadf98af77e
Emil VATAI <emil.vatai@gmail.com>
2020-05-06 17:05:38 +0900

    Add test code to `gen_images()`

--- benchmarker/modules/problems/images_randomized.py
+++ benchmarker/modules/problems/images_randomized.py
@@ -38,3 +38,22 @@ def gen_images(params):
     X = np.random.random(shape).astype(np.float32)
     Y = np.random.random(shape[:2]).astype(np.int64)
     return X, Y
+
+
+def test():
+    params = {
+        "batch_size": 32,
+        "problem": {
+            "name": "conv2d",
+            "cnt_batches_per_epoch": 16,
+            "size": [10, 20, 30, 40],
+        },
+    }
+    X, Y = gen_images(params)
+
+    assert X.shape == (16, 32, 20, 30, 40)
+    assert Y.shape == (16, 32)
+
+
+if __name__ == "__main__":
+    test()
