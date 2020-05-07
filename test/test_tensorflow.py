6ee35247998ca6b42408754757e16959c2799fa6
Emil VATAI <emil.vatai@gmail.com>
2020-05-07 23:54:45 +0900

    Make things silent

--- test/test_tensorflow.py
+++ test/test_tensorflow.py
@@ -2,12 +2,17 @@ import unittest
 import logging
 from .helpers import run_module
 
+import os
+from io import StringIO
+from contextlib import redirect_stderr, redirect_stdout
+
 logging.basicConfig(level=logging.DEBUG)
 
 
 class TensorflowTests(unittest.TestCase):
     # TODO(vatai): Run it quietly
     def setUp(self):
+        os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
         self.name = "benchmarker"
         self.imgnet_args = [
             "--framework=tensorflow",
@@ -16,7 +21,11 @@ class TensorflowTests(unittest.TestCase):
         ]
 
     def test_vgg16(self):
-        run_module(self.name, "--problem=vgg16", *self.imgnet_args)
+        sio = StringIO()
+        with redirect_stdout(sio), redirect_stderr(sio):
+            run_module(self.name, "--problem=vgg16", *self.imgnet_args)
 
     def test_resnet50(self):
-        run_module(self.name, "--problem=resnet50", *self.imgnet_args)
+        sio = StringIO()
+        with redirect_stdout(sio), redirect_stderr(sio):
+            run_module(self.name, "--problem=resnet50", *self.imgnet_args)
