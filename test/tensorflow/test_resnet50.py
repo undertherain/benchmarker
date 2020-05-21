a08b94c22b23e5d5e0f731a3538a718187fa8aad
Emil VATAI <emil.vatai@gmail.com>
2020-05-21 20:51:33 +0900

    Clean up Tensorflow tests

--- test/tensorflow/test_resnet50.py
+++ test/tensorflow/test_resnet50.py
@@ -1,16 +1,14 @@
-import unittest
 import logging
-from .helpers import run_module
 import os
+import unittest
+
+from ..helpers import run_module
 
 logging.basicConfig(level=logging.DEBUG)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-# vatai: I think loading TF here makes the tests a little faster :-/
-import tensorflow as tf
 
-
-class TensorflowTests(unittest.TestCase):
+class Resnet50Tests(unittest.TestCase):
     def setUp(self):
         self.name = "benchmarker"
         self.imgnet_args = [
@@ -18,9 +16,6 @@ class TensorflowTests(unittest.TestCase):
             "--problem_size=4",
             "--batch_size=2",
         ]
-
-    def test_vgg16(self):
-        run_module(self.name, "--problem=vgg16", *self.imgnet_args)
 
     def test_resnet50(self):
         run_module(self.name, "--problem=resnet50", *self.imgnet_args)
