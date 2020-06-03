1d7cad3d23e8540557a2838dcc21d4e33348de07
Emil VATAI <emil.vatai@gmail.com>
2020-06-03 10:44:14 +0900

    Fix typo(s) (params is not used)
    And also test the bot ;)

--- benchmarker/modules/problems/vgg16/tensorflow.py
+++ benchmarker/modules/problems/vgg16/tensorflow.py
@@ -2,7 +2,7 @@ from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.optimizers import SGD
 
 
-def get_kernel(parsm, unparsed_args):
+def get_kernel(params, unparsed_args):
     model = VGG16(weights=None)
     optimizer = SGD()
     model.compile(loss='categorical_crossentropy',
