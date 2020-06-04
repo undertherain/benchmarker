905a57951f51a807d0cfc5aa623c9015f668e173
Emil VATAI <emil.vatai@gmail.com>
2020-06-04 20:15:15 +0900

    Write `get_data` for convolutions
    This function is a port of `gen_images()` from `images_randomized.py`,
    tailored specifically for the convolution layers.  Also use it in the
    convXd `data.py` files.

---
+++ benchmarker/util/data/synthetic/conv.py
@@ -0,0 +1,30 @@
+"""Generate synthetic data for conv layers."""
+
+import re
+
+import numpy as np
+
+
+def _get_conv_dim(name):
+    """Return X in convXd."""
+    m = re.match("conv(.)d", name)
+    if m:
+        groups = m.groups()
+        dim = int(groups[0])
+        assert 1 <= dim and dim <= 3
+        return dim
+    else:
+        raise ValueError("Wrong input")
+
+
+def get_data(params):
+    """Return np.arrays."""
+    problem = params["problem"]
+    dim = _get_conv_dim(problem["name"])
+    assert len(problem["size"]) == dim + 2
+
+    shape = (problem["cnt_batches_per_epoch"], params["batch_size"])
+    shape = shape + problem["size"][1:]
+    X = np.random.random(shape).astype(np.float32)
+    Y = np.random.random(shape[:2]).astype(np.int64)
+    return X, Y
