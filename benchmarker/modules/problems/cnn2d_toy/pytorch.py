213ad0a7e6e78c2042377f3494b74e0d445f8dec
Sasha <alexander.drozd@gmail.com>
2020-06-09 11:24:48 +0900

    train and inf version

--- benchmarker/modules/problems/cnn2d_toy/pytorch.py
+++ benchmarker/modules/problems/cnn2d_toy/pytorch.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 
 class Net(nn.Module):
-    def __init__(self, train=True):
+    def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=2)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=2)
@@ -17,6 +17,24 @@ class Net(nn.Module):
         h = F.relu(h)
         h = self.dense1(h)
         return h
+
+
+class Net4Inference(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = Net()
+
+    def __call__(self, x):
+        return F.softmax(self.net(x))
+
+
+class Net4Train(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = Net()
+
+    def __call__(self, x, t):
+        return f.crossentopy(self.net(x), t)
 
 
 def get_kernel(params, unparsed_args=None):
