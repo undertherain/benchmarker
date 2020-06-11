4fc1cd3e3b225b981521cf321f8c4441f45c3252
Emil VATAI <emil.vatai@gmail.com>
2020-06-11 19:43:35 +0900

    Add training and inference test

--- test/pytorch/test_cnn.py
+++ test/pytorch/test_cnn.py
@@ -9,11 +9,15 @@ logging.basicConfig(level=logging.DEBUG)
 class PytorchCNNTest(unittest.TestCase):
     def setUp(self):
         self.name = "benchmarker"
-        self.imgnet_args = [
+        self.args = [
+            "--problem=cnn2d_toy",
             "--framework=pytorch",
             "--problem_size=4",
             "--batch_size=2",
         ]
 
-    def test_vgg16(self):
-        run_module(self.name, "--problem=cnn2d_toy", *self.imgnet_args)
+    def test_cnn2d_inference(self):
+        run_module(self.name, "--mode=inference", *self.args)
+
+    def test_cnn2d_training(self):
+        run_module(self.name, "--mode=training", *self.args)
