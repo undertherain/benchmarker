9e46372d268bb390613bebdf51e730ed86d8875a
Emil VATAI <emil.vatai@gmail.com>
2020-06-11 16:54:51 +0900

    Download first N=20 images (return list of files)

--- benchmarker/util/data/download/coco.py
+++ benchmarker/util/data/download/coco.py
@@ -2,17 +2,28 @@ from pathlib import Path
 
 from pycocotools.coco import COCO
 
+from helpers import get_file
+
 
 def get_data(params, N=20):
     BASE = Path("~/.cache/benchmarker/").expanduser()
-    COCODIR = BASE.joinpath("data/coco/annotations/instances_val2017.json")
-    coco = COCO(COCODIR)
+    COCODIR = BASE.joinpath("data/coco/")
+    coco = COCO(COCODIR.joinpath("annotations/instances_val2017.json"))
     imgIds = coco.getImgIds()[:N]
     imgStructs = coco.loadImgs(imgIds)
     # each element has keys: ['license', 'file_name', 'coco_url',
     # 'height', 'width', 'date_captured', 'flickr_url', 'id']
-    imgUrls = map(lambda struct: struct["coco_url"], imgStructs)
-    return list(imgUrls)
+
+    def _get_file(imgStruct):
+        from os.path import basename
+
+        imgUrl = imgStruct["coco_url"]
+        fname = basename(imgUrl)
+        return get_file(fname, imgUrl, cache_subdir="images", cache_dir=COCODIR)
+
+    file_paths = map(_get_file, imgStructs)
+
+    return list(file_paths)
 
 
 if __name__ == "__main__":
