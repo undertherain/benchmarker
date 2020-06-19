f699ba51b15cf3c02c51205b4c8e8da65642a7fd
Sasha <alexander.drozd@gmail.com>
2020-06-19 12:20:37 +0900

    test train bert

--- test/pytorch/test_bert.py
+++ test/pytorch/test_bert.py
@@ -10,7 +10,7 @@ class PytorchBertTest(unittest.TestCase):
     def setUp(self):
         self.name = "benchmarker"
 
-    def test_bert(self):
+    def test_bert_inference(self):
         run_module(
             self.name,
             "--framework=pytorch",
@@ -19,4 +19,15 @@ class PytorchBertTest(unittest.TestCase):
             "--batch_size=2",
             "--nb_epoch=1",
             "--mode=inference",
+        )
+
+    def test_bert_training(self):
+        run_module(
+            self.name,
+            "--framework=pytorch",
+            "--problem=bert",
+            "--problem_size=4,8",
+            "--batch_size=2",
+            "--nb_epoch=1",
+            "--mode=training",
         )
