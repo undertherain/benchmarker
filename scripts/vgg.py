2f2203962f9b1773afa9b03c9b334991124c5e9b
Sasha <alexander.drozd@gmail.com>
2020-06-22 07:39:08 +0900

    fix

--- scripts/vgg.py
+++ scripts/vgg.py
@@ -7,12 +7,12 @@ def main():
     params["framework"] = "pytorch"
     params["problem"] = "vgg16"
     params["mode"] = "training"
-    params["gpus"] = "0"
     for batch_size in range(1, 256):
         params["batch_size"] = batch_size
         prob_size = params["batch_size"] * 4
         params["problem_size"] = f"{prob_size}"
         print(batch_size)
+        params["gpus"] = "0"
         run(params)
         params.pop("gpus")
         params["backend"] = "DNNL"
