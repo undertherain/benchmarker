1115d7fc5a7ea0da5b4d5c5187243cb163987281
Sasha <alexander.drozd@gmail.com>
2020-06-22 11:20:47 +0900

    fix bert

--- scripts/bert.py
+++ scripts/bert.py
@@ -7,6 +7,7 @@ def main():
     params["problem"] = "bert"
     params["mode"] = "training"
     for batch_size in fast_batches:
+        params["batch_size"] = batch_size
         print(batch_size)
         run_on_all_backends(params)
 
