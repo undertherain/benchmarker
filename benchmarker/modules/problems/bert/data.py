c3afbda326d26cf6dd0dea4b036f0b59ae02b570
Sasha <alexander.drozd@gmail.com>
2020-06-22 09:44:46 +0900

    default sequence length for bert

--- benchmarker/modules/problems/bert/data.py
+++ benchmarker/modules/problems/bert/data.py
@@ -2,6 +2,8 @@ import numpy as np
 
 
 def get_data(params):
+    if isinstance(params["problem"]["size"], int):
+        params["problem"]["size"] = (params["problem"]["size"], 128)
     assert params["problem"]["size"][0] % params["batch_size"] == 0
     params["problem"]["len_sequence"] = params["problem"]["size"][1]
     cnt_batches = params["problem"]["size"][0] // params["batch_size"]
