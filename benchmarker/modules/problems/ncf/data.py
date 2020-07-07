4d93a7113208aaa28dbd5e578b18558c80716690
Emil VATAI <emil.vatai@gmail.com>
2020-07-07 12:49:58 +0900

    Cleanup ncf/data.py

--- benchmarker/modules/problems/ncf/data.py
+++ benchmarker/modules/problems/ncf/data.py
@@ -3,14 +3,11 @@ import numpy as np
 
 def get_data(params):
     """generates synthetic dataset"""
-    # input size -> nb_users, nb_items, factors, layers
-    # transform into (nb_users, nb_items, factors, layers) x cnt_batches
     nb_users = params["problem"]["nb_users"]
     nb_items = params["problem"]["nb_items"]
     batch_size = params["batch_size"]
-    assert params["problem"]["size"] % batch_size == 0
     cnt_batches = params["problem"]["size"] // batch_size
-    # shape = (cnt_bathces, 2,
+    assert params["problem"]["size"] % batch_size == 0
 
     X = []
     Y = []
@@ -24,12 +21,4 @@ def get_data(params):
 
     X = np.array(X)
     Y = np.array(Y)
-    print("param[problems]", params["problem"])
-    print("X.shape", X.shape)
-    print("Y.shape", Y.shape)
-    # mp.array([ (randint(0..cnt_users), randint(0..cnt_items), rand(0_max_score)) ]  for _ in range size_data])
-    # resahpe(cnt_batches, batch_size, -1)
-    # data  = one_item x batch_size x cnt_batches
-    # X = [np.array([np.random.random(shape).astype(np.int64), np.random.random(shape).astype(np.int64)]) for _ in range(cnt_batches)]
-    # Y = np.ones((cnt_batches, params["batch_size"]))
     return X, Y
