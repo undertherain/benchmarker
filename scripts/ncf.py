59de39c32bce40a1f043acfd1cae0005cd64f3f6
acc12262dj <acc12262dj@es2.abci.local>
2020-07-09 14:24:45 +0900

    Add multipliers and a cpu and gpu loop

--- scripts/ncf.py
+++ scripts/ncf.py
@@ -7,8 +7,13 @@ def main():
     params["problem"] = "ncf"
     params["mode"] = "training"
     params["framework"] = "pytorch"
-    params["nb_users"] = "138000"
-    params["nb_items"] = "27000"
+    params["nb_users"] = str(16*138000)
+    params["nb_items"] = str(32*27000)
+    for batch_size in fast_batches:
+        params["batch_size"] = batch_size
+        params["problem_size"] = 4 * batch_size
+        print(batch_size)
+        run(params)
     params["gpus"] = "0"
     for batch_size in fast_batches:
         params["batch_size"] = batch_size
