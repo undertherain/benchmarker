7dfb2fd745d7f5672698f5a67192aeb97ad0d881
Emil VATAI <emil.vatai@gmail.com>
2020-07-20 11:59:52 +0900

    Guess get_data()

--- benchmarker/modules/problems/cosmoflow/data.py
+++ benchmarker/modules/problems/cosmoflow/data.py
@@ -1,10 +1,16 @@
-### !!! STUB !!! ###
 import numpy as np
 
 
 def get_data(params):
-    shape = (1, 2)
-    num_cls = 3
+    shape = (params["problem"]["cnt_batches_per_epoch"], params["batch_size"])
+    shape += params["input_shape"]
     X = np.random.random(shape).astype(np.float32)
-    Y = np.random.randint(0, num_cls, shape[:2])
+
+    shape = (
+        params["problem"]["cnt_batches_per_epoch"],
+        params["batch_size"],
+        params["target_size"],
+    )
+    Y = np.random.random(shape).astype(np.float32)
+
     return X, Y
