f8192ba92619957029e48a88dacd5f1b0ad05a9e
Sasha <alexander.drozd@gmail.com>
2020-07-21 13:58:59 +0900

    bettwer wording for script

--- scripts/gemm.sh
+++ scripts/gemm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # HACK this does not work with pip-installed benchmarker
 # because binaries are not in site_packages, alse can't use run script
-# as temp solution - this sh script to be copied to root folder manually when needed
+# as a temp solution - this script to be copied to benchmarker's root folder manually when needed
 for framework in cblas torch numpy
 do
     python3 -m benchmarker --problem=gemm --framework=$framework --problem_size=1024,1024,1024
