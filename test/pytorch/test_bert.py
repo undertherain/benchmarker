19d0b1268447599990f773ec80d588fcc14ca5ce
Sasha <alexander.drozd@gmail.com>
2020-09-04 09:55:21 +0900

    tests

--- test/pytorch/test_bert.py
+++ test/pytorch/test_bert.py
@@ -21,3 +21,20 @@ class PytorchBertTest(unittest.TestCase):
 
     def test_bert_training(self):
         run(self.args + ["--mode=training"])
+
+
+class PytorchBertLargeTest(unittest.TestCase):
+    def setUp(self):
+        self.args = [
+            "--framework=pytorch",
+            "--problem=bert_large",
+            "--problem_size=4,8",
+            "--batch_size=2",
+            "--nb_epoch=1",
+        ]
+
+    def test_bert_inference(self):
+        run(self.args + ["--mode=inference"])
+
+    def test_bert_training(self):
+        run(self.args + ["--mode=training"])
