fcfb50d189be8626fcc957910a9856baeb23b733
Sasha <alexander.drozd@gmail.com>
2020-09-04 09:51:45 +0900

    bert large use helpers

--- benchmarker/modules/problems/bert_large/pytorch.py
+++ benchmarker/modules/problems/bert_large/pytorch.py
@@ -1,31 +1,10 @@
-import torch.nn as nn
 from transformers import AutoModelForSequenceClassification, AutoConfig
+from benchmarker.modules.problems.helpers_torch_bert import BertInference, BertTraining
 
 
 config = AutoConfig.from_pretrained(
     "bert-large-uncased",
     num_labels=3)
-
-
-class BertTraining(nn.Module):
-    def __init__(self, net):
-        super().__init__()
-        self.net = net
-
-    def __call__(self, x, t):
-        loss, _logits = self.net(input_ids=x,
-                                 labels=t)
-        return loss
-
-
-class BertInference(nn.Module):
-    def __init__(self, net):
-        super().__init__()
-        self.net = net
-
-    def __call__(self, x):
-        logits = self.net(input_ids=x)
-        return logits
 
 
 def get_kernel(params, unparsed_args=None):
