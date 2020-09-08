768006246b0e5ead53500ee96aa9ff6f89c43d2d
Sasha <alexander.drozd@gmail.com>
2020-09-08 14:58:46 +0900

    add missing file

---
+++ benchmarker/modules/problems/gru/params.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def set_extra_params(params, unparsed_args):
+    parser = argparse.ArgumentParser(description='Benchmark kernel')
+    parser.add_argument('--cnt_units', type=int, default=512)
+    parser.add_argument('--cnt_layers', type=int, default=1)
+    parser.add_argument('--bidirectional', type=bool, default=False)
+    args = parser.parse_args(unparsed_args)
+    params["problem"].update(vars(args))
