d5aafac1b87e940e09f9346f7eda3ef614e778cf
Sasha <alexander.drozd@gmail.com>
2020-09-08 15:39:56 +0900

    fix bert

--- benchmarker/modules/problems/bert_custom/params.py
+++ benchmarker/modules/problems/bert_custom/params.py
@@ -7,7 +7,7 @@ def set_extra_params(params, unparsed_args):
     parser.add_argument("--target_size", default=4)
     parser.add_argument("--dropout", default=0)
 
-    args = parser.args(unparsed_args)
+    args = parser.parse_args(unparsed_args)
     # TODO(Alex): this should be in problem sub-dict
     params["input_shape"] = ast.literal_eval(args.input_shape)
     params["target_size"] = args.target_size
