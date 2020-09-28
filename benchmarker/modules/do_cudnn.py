99dcc08ccb9ce68221ed170c190dbe9497587af1
Emil VATAI <emil.vatai@gmail.com>
2020-09-28 18:09:53 +0900

    Start params processing

--- benchmarker/modules/do_cudnn.py
+++ benchmarker/modules/do_cudnn.py
@@ -1,6 +1,6 @@
 """Module contains the interface for all deep learning modules"""
-import argparse
-import os
+import importlib
+from pathlib import Path
 
 from benchmarker.util.abstractprocess import Process
 
@@ -9,42 +9,39 @@ class Benchmark:
     """Interface for all deep learning modules"""
 
     def __init__(self, params, remaining_args=None):
+        path_params = f"benchmarker.modules.problems.{params['problem']['name']}.params"
+        try:
+            module_params = importlib.import_module(path_params)
+            module_params.set_extra_params(params, remaining_args)
+        except ImportError:
+            assert remaining_args == []
+        assert params["problem"]["name"] == "conv", (
+            "only conv problem is defined for this framework, "
+            f"not {params['problem']['name']}"
+        )
+        assert (
+            "nb_gpus" in params and params["nb_gpus"] == 1
+        ), "cuDNN requires exactly one GPU"
         self.params = params
-        # parser = argparse.ArgumentParser(description="gemm extra args")
-        # parser.add_argument("--precision", default="FP32")
-        # args, remaining_args = parser.parse_known_args(remaining_args)
-        # args = parser.parse_args(remaining_args)
-        # params["problem"]["precision"] = args.precision
-        # params["path_out"] = os.path.join(
-        #     params["path_out"], params["problem"]["precision"]
-        # )
-
-        params["problem"]["size"] = 42  # self.matrix_size
-
-        if params["problem"]["name"] != "conv":
-            raise Exception(
-                "only conv problem is defined for this framework, "
-                f"not {params['problem']['name']}"
-            )
 
     def run(self):
-        if "nb_gpus" in self.params:
-            if self.params["nb_gpus"] == 0:
-                raise Exception("cudnn requires at least one GPU")
-        # size = " ".join(map(str, self.params["problem"]["size"]))
-        path_binary = os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "problems/conv/main"
-        )
-        if not os.path.isfile(path_binary):
-            raise (RuntimeError(f"{path_binary} not found, run make manually"))
-        gpu_id, nb_dim, algo = 0, 2, 1
+        bin_path = f"problems/{self.params['problem']['name']}/main"
+        cmd_binary = Path(__file__).parent.joinpath(bin_path)
+        if not cmd_binary.is_file():
+            raise (RuntimeError(f"{cmd_binary} not found, run make manually"))
+
         batch_size, in_chan, out_chan = 1, 3, 3
         in_dims = [578, 549]
         ker_dims = [3, 3]
         ker_pads = [1, 1]
         ker_stride = [1, 1]
         ker_dilation = [1, 1]
-        command = [path_binary, gpu_id, algo, nb_dim]
+
+        problem = self.params["problem"]
+        command = [cmd_binary]
+        command.append(self.params["gpus"][0])
+        command.append(problem["cudnn_conv_algo"])
+        command.append(problem["nb_dims"])
         command += [batch_size, in_chan, out_chan]
         command += in_dims
         command += ker_dims
@@ -52,10 +49,10 @@ class Benchmark:
         command += ker_stride
         command += ker_dilation
         command = list(map(str, command))
-        print(f"command[] = {command}")
+
+        print(self.params)
         process = Process(command=command)
         result = process.get_output()
-        print(result)
         std_out = result["out"]
         print(std_out.strip())
         elapsed_time = float(std_out.strip())
--- benchmarker/modules/problems/conv/params.py
+++ benchmarker/modules/problems/conv/params.py
@@ -1,15 +1,26 @@
 import argparse
+from ast import literal_eval
 
 
 def set_extra_params(params, unparsed_args):
     parser = argparse.ArgumentParser(description="Benchmark cuDNN conv kernel")
     parser.add_argument("--cudnn_conv_algo", type=int, default=2)
     parser.add_argument("--nb_dims", type=int, default=2)
+    parser.add_argument("--cnt_samples", type=int, default=8)
+    parser.add_argument("--input_size", default=(100, 100))
+    parser.add_argument("--input_channels", type=int, default=3)
     parser.add_argument("--size_kernel", default=3)
     parser.add_argument("--cnt_filters", default=64)
     parser.add_argument("--stride", default=1)
     parser.add_argument("--dilation", default=1)
     parser.add_argument("--padding", default=1)
     args = parser.parse_args(unparsed_args)
-    print(f"args.nb_dims = {args.nb_dims}")
+
+    if isinstance(args.input_size, str):
+        args.input_size = literal_eval(args.input_size)
+    assert len(args.input_size) == args.nb_dims, (
+        f"input_size has {len(args.input_size)} number of elements, "
+        f"while it should be nb_dims = {args.nb_dims}: "
+        "dim1, dim2, ..."
+    )
     params["problem"].update(vars(args))
