42bf323501be8fa34ffbe1c8534d29f674dce5b7
Shweta Salaria <salaria.shweta@gmail.com>
2020-10-12 22:09:57 +0900

    save json to 'flops' folder

--- scripts/conv2d.py
+++ scripts/conv2d.py
@@ -5,7 +5,9 @@ import os
 import time
 import json
 import re
-import sys 
+import sys
+from benchmarker.util.io import save_json
+from benchmarker.util.cute_device import get_cute_device_str
 
 batchsize = [32]
 epoch = 10 
@@ -76,11 +78,16 @@ def main():
             #Run benchmarker with params and get output as dict
             output_string, error_string = run(params, command)
             output_dict = json.loads(output_string)
-            print(output_dict)
+            #print(output_dict)
 
-            #Collect counters, add to the dict
+            #Collect counters, add to the dict, save as json
             dict_with_ops = get_counters(params, output_dict, command)
-            print(dict_with_ops)
+            cute_device = get_cute_device_str(dict_with_ops["device"]).replace(" ", "_")
+            dict_with_ops["path_out"] = os.path.join("./flops",dict_with_ops["problem"]["name"],dict_with_ops["mode"], cute_device)
+            #print(dict_with_ops)
+            save_json(dict_with_ops)
+
 
 if __name__ == "__main__":
+    print(sys.path)
     main()
