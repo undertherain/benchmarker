86c41505b614fa46d74008caac7ef83de7c191bd
Shweta Salaria <salaria.shweta@gmail.com>
2020-10-12 10:04:21 +0900

    refactor

--- scripts/conv2d.py
+++ scripts/conv2d.py
@@ -1,39 +1,71 @@
 #!/usr/bin/env python3
 import subprocess
-import tempfile
 import pandas
 import os
 import time
+import json
+import re
 
-basedir = "./flops"
-batchsize = [32] 
+batchsize = [32]
+epoch = 10 
 
-def run(params, command, output_file):
-    with open(output_file, "a") as f:
-        out_file = f
-        command.extend(("python3", "-m", "benchmarker", "--no_cudnn_benchmark"))
-        for key in params:
-            command.append(f"--{key}")
-            command.append(str(params[key]))
-        print(command)
-        proc = subprocess.Popen(command, stdout=out_file, stderr=out_file)
+perf_counters_multipliers = {'r5302c7': 1, 
+                             'r5308c7': 4,
+                             'r5320c7': 8}
+
+def run(params, command):
+    for key, val in params.items():
+        command.append(f"--{key}={val}")
+    print(command)
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc.wait()
+    out = proc.stdout.read().decode()
+    err = proc.stderr.read().decode()
+    if proc.returncode == 0:
+        output_dict = json.loads(out)
+        return output_dict
+    else:
+        print(out)
+        print(err)
+
+
+def get_counters(params, output_dict):
+    output_dict["flop_measured"] = 0
+    for counter in perf_counters_multipliers:
+        perf_command = ["perf", "stat" , "-e", counter, "python3", "-m", "benchmarker", "--no_cudnn_benchmark"]
+        for key, val in params.items():
+            perf_command.append(f"--{key}={val}")
+        print(perf_command)
+        proc = subprocess.Popen(perf_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         proc.wait()
-        out_file.close()
+        out = proc.stdout.read().decode()
+        err = proc.stderr.read().decode()
+        print(err)
+        #if proc.returncode != 0:
+        #print(out)
+        #print(err)
+        if err:
+            match_exp = re.compile('[\d|\,]+\s+' + counter).search(err)
+            if match_exp:
+                match_list = match_exp.group().split()
+                cntr_value = int(match_list[0].replace(',',''))
+                output_dict["flop_measured"] += perf_counters_multipliers[counter]*cntr_value
+    print(output_dict)
+
+            
 
 def main():
     params = {}
     params["framework"] = "pytorch"
     params["mode"] = "inference"
 
-    specs = pandas.read_csv("./conv2d_specs.csv")
-    print(specs)
+    specs = pandas.read_csv("./conv2d.csv")
     for size in batchsize: 
         params["batch_size"] = size
         for row in specs.iterrows():
             spec = row[1]
             params["problem"] = spec["conv_type"]
             prob_size = params["batch_size"] * 4
-            print(spec)
 
             if params["problem"] == 'conv2d':
                 params["problem_size"] = (f"{prob_size},"
@@ -46,37 +78,17 @@ def main():
             params["stride"] = spec["stride"]
             params["dilation"] = spec["dilation"]
             params["padding"] = spec["padding"]
-            #print (params)
-           
-            filename = (f"{params['problem']}"
-			f"_in{spec['in_channels']}"
-			f"x{spec['in_width']}"
-			f"x{spec['in_height']}"
-                        f"_k{params['cnt_filters']}"
-                        f"_s{params['stride']}"
-                        f"_p{params['padding']}"
-                        f"_bs{params['batch_size']}")
-          
-            out_dir = os.path.join(basedir, params["problem"], "perf")
-            if not os.path.exists(out_dir):
-                os.makedirs(out_dir)  
-            output_file = os.path.join(out_dir, filename) 
-            command = []
-            params["backend"] = "DNNL"
-            run(params, command, output_file)
+            params["nb_epoch"] = epoch 
+            #print(params)
+         
+            command = ["python3", "-m", "benchmarker", "--no_cudnn_benchmark"] 
 
-            #Count FP_ARITH:SCALAR_SINGLE
-            command = ["perf", "stat","-e", "r5302c7"]
-            run(params, command, output_file)
+            #Run benchmarker with params and get output as dict
+            output_dict = run(params, command)
+            print(output_dict)
 
-            #Count FP_ARITH:128B_PACKED_SINGLE   
-            command = ["perf", "stat","-e", "r5308c7"]
-            run(params, command, output_file)
-
-            #Count FP_ARITH:256B_PACKED_SINGLE
-            command = ["perf", "stat","-e", "r5320c7"]
-            run(params, command, output_file)
-
+            #Collect counters, add to the dict
+            get_counters(params, output_dict)
 
 if __name__ == "__main__":
     main()
