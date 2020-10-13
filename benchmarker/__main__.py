4c73fc4b86458eaa3835d718b6be15b465813f34
Sasha <alexander.drozd@gmail.com>
2020-10-13 10:48:35 +0900

    stub of collectin perf

--- benchmarker/__main__.py
+++ benchmarker/__main__.py
@@ -3,11 +3,19 @@
 import sys
 
 from .benchmarker import run
+from benchmarker.util import abstractprocess
 
 
 def main():
     """CLI entry point function"""
     run(sys.argv[1:])
+    # put corrected args to the command
+    # run in a loop to collect counters
+    command = "run benchmarker"
+    proc = abstractprocess.Process("local", command=command)
+    result = proc.get_output()
+    # add to results
+    # save
 
 
 if __name__ == "__main__":
