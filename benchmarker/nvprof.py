08d875dfe605975eb79b29368bf1ec67c9233712
Shweta Salaria <salaria.shweta@gmail.com>
2020-10-21 09:33:58 +0900

    fix naming regex

--- benchmarker/nvprof.py
+++ benchmarker/nvprof.py
@@ -2,8 +2,8 @@ import re
 import subprocess
 from benchmarker.util import abstractprocess
 
-exp1_re = re.compile('(\d+)\s+flop\_count\_sp\s+Floating\s+Point\s+Operations\(Single Precision\)\s+(?:(\d+)\s+){3}')
-exp2_re = re.compile('(\d+)\s+flop\_count\_sp\s+Floating\s+Point\s+Operations\(Single Precision\)\s+(?:(\d+\.\d+e[\+|\-]\d+)\s+){3}')
+nvprof_expr1_re = re.compile('(\d+)\s+flop\_count\_sp\s+Floating\s+Point\s+Operations\(Single Precision\)\s+(?:(\d+)\s+){3}')
+nvprof_expr2_re = re.compile('(\d+)\s+flop\_count\_sp\s+Floating\s+Point\s+Operations\(Single Precision\)\s+(?:(\d+\.\d+e[\+|\-]\d+)\s+){3}')
 
 
 def get_nvprof_counters(command):
@@ -11,13 +11,13 @@ def get_nvprof_counters(command):
     proc = abstractprocess.Process("local", command=nvprof_command + command)
     process_err = proc.get_output()["err"]
     #print(process_err)
-    match_exp1 = exp1_re.findall(process_err)
-    #print(match_exp1)
-    output_exp1 = [int(x) * int(y) for x, y in match_exp1]
-    match_exp2 = exp2_re.findall(process_err)
-    #print(match_exp2)
-    output_exp2 = [int(x) * int(float(y)) for x, y in match_exp2]
-    output = output_exp1 + output_exp2
+    match_expr1 = nvprof_expr1_re.findall(process_err)
+    #print(match_expr1)
+    output_expr1 = [int(x) * int(y) for x, y in match_expr1]
+    match_expr2 = nvprof_expr2_re.findall(process_err)
+    #print(match_expr2)
+    output_expr2 = [int(x) * int(float(y)) for x, y in match_expr2]
+    output = output_expr1 + output_expr2
     #print(output)
     gflop_measured = sum(output) / 10 ** 9
     return gflop_measured
