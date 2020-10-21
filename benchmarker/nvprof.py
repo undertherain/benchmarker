e974f94e869b9deb2b41cf755f6f97467d8ffd05
Shweta Salaria <salaria.shweta@gmail.com>
2020-10-21 16:42:21 +0900

    final fix to regexp

--- benchmarker/nvprof.py
+++ benchmarker/nvprof.py
@@ -2,7 +2,7 @@ import re
 import subprocess
 from benchmarker.util import abstractprocess
 
-flops_in_whole_number = re.compile('(\d+)\s+flop\_count\_sp\s+Floating\s+Point\s+Operations\(Single Precision\)\s+(?:(\d+)\s+){3}')
+flops_as_integer = re.compile('(\d+)\s+flop\_count\_sp\s+Floating\s+Point\s+Operations\(Single Precision\)\s+(?:(\d+)\s+){3}')
 flops_in_scientific_notation = re.compile('(\d+)\s+flop\_count\_sp\s+Floating\s+Point\s+Operations\(Single Precision\)\s+(?:(\d+\.\d+e[\+|\-]\d+)\s+){3}')
 
 
@@ -11,13 +11,13 @@ def get_nvprof_counters(command):
     proc = abstractprocess.Process("local", command=nvprof_command + command)
     process_err = proc.get_output()["err"]
     # print(process_err)
-    match_expr1 = flops_in_whole_number.findall(process_err)
-    # print(match_expr1)
-    output_expr1 = [int(x) * int(y) for x, y in match_expr1]
-    match_expr2 = flops_in_scientific_notation.findall(process_err)
-    # print(match_expr2)
-    output_expr2 = [int(x) * int(float(y)) for x, y in match_expr2]
-    output = output_expr1 + output_expr2
+    matched_flops_int = flops_as_integer.findall(process_err)
+    # print(matched_flops_int)
+    total_flops_int = [int(x) * int(y) for x, y in matched_flops_int]
+    matched_flops_scientific_notation = flops_in_scientific_notation.findall(process_err)
+    # print(matched_flops_scientific_notation)
+    total_flops_scientific_notation = [int(x) * int(float(y)) for x, y in matched_flops_scientific_notation]
+    output = total_flops_int + total_flops_scientific_notation
     # print(output)
     gflop_measured = sum(output) / 10 ** 9
     return gflop_measured
