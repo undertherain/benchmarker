a94a4b30978cae5255d356f60208422c533341d0
Emil VATAI <emil.vatai@gmail.com>
2020-10-23 19:02:16 +0900

    [WIP] Factor out `fapp` runs

--- benchmarker/fapp.py
+++ benchmarker/fapp.py
@@ -1,23 +1,32 @@
 import re
 import subprocess
+from tempfile import TemporaryDirectory
 
 from benchmarker.util import abstractprocess
+
+
+def run_fapp_profiler(fapp_dir, command):
+    # fapp -C -d ./prof_${APP}_rep${REP} -Hevent=pa${REP} ./${APP}
+    fapp_measure_cmd = ["fapp", "-C", "-d"]
+    fapp_measure_cmd += [fapp_dir, "-Hevent=pa{rep}"]
+    proc = abstractprocess.Process("local", command=fapp_measure_cmd + command)
+
+
+def gen_fapp_csv(fap_dir, csv_file):
+    # fapp -A -tcsv -o ${APP}_reps/pa$REP.csv -d ./prof_${APP}_rep${REP} -Icpupa
+    fapp_gen_csv_cmd = ["fapp", "-A", "-tcsv"]
+    fapp_gen_csv_cmd += ["-o", csv_file] + ["-d", fapp_dir, "-Icpupa"]
+    proc = abstractprocess.Process("local", command=fapp_gen_csv_cmd)
 
 
 def get_counters(command):
     flop_measured = 0
     for counter in perf_counters_multipliers:
         for rep in [1, 8]:
-            fapp_output = "./prof"
-            csv_output = "./csvs/"
-            # fmt: off
-            # fapp -C -d ./prof_${APP}_rep${REP} -Hevent=pa${REP} ./${APP}
-            fapp_measure_cmd = ["fapp", "-C", "-d", f"./prof_rep{rep} -Hevent=pa{rep}"]
-            # fapp -A -tcsv -o ${APP}_reps/pa$REP.csv -d ./prof_${APP}_rep${REP} -Icpupa
-            fapp_gen_csv_cmd = ["fapp", "-A", "-tcsv", "-o", "reps/pa{rep}.csv", "-d", "./prof_rep{rep}", "-Icpupa",]
-            # fmt: on
-            proc = abstractprocess.Process("local", command=fapp_measure_cmd + command)
-
+            fapp_dir = f"./prof{rep}"
+            csv_file = f"./csvs/pa{rep}.csv"
+            run_fapp_profiler(fapp_dir, command)
+            gen_fapp_csv(fap_dir, csv_file)
             # process_err = proc.get_output()["err"]
             # print(process_err)
         # delete tmp files
