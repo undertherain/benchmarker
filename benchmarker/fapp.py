d65b321908805d8a50d5ab0470cc1796755cb268
Emil VATAI <emil.vatai@gmail.com>
2020-10-27 11:46:19 +0900

    Add debug instrumentation

--- benchmarker/fapp.py
+++ benchmarker/fapp.py
@@ -9,18 +9,22 @@ def run_fapp_profiler(fapp_dir, command):
     # fapp -C -d ./prof_${APP}_rep${REP} -Hevent=pa${REP} ./${APP}
     fapp_measure_cmd = ["fapp", "-C", "-d"]
     fapp_measure_cmd += [fapp_dir, "-Hevent=pa{rep}"]
-    abstractprocess.Process("local", command=fapp_measure_cmd + command)
+    cmd = fapp_measure_cmd + command
+    print(cmd)
+    abstractprocess.Process("local", command=cmd)
 
 
 def gen_fapp_csv(fapp_dir, csv_file):
     # fapp -A -tcsv -o $APP_reps/pa$REP.csv -d ./prof_$APP_rep$REP -Icpupa
     fapp_gen_csv_cmd = ["fapp", "-A", "-tcsv"]
     fapp_gen_csv_cmd += ["-o", csv_file] + ["-d", fapp_dir, "-Icpupa"]
+    print(fapp_gen_csv_cmd)
     abstractprocess.Process("local", command=fapp_gen_csv_cmd)
 
 
 def get_power_total_and_detail(command):
     csv_dir = Path("csvs")
+    csv_dir.mkdir()
     for rep in [1, 8]:
         with TemporaryDirectory(suffix=str(rep)) as fapp_dir:
             csv_file = csv_dir.joinpath(f"pa{rep}.csv")
