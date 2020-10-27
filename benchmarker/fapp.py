67d5d034a3ad4e62cc5e0404ecd21e6247c3133a
Emil VATAI <emil.vatai@gmail.com>
2020-10-27 12:23:13 +0900

    Add missing `rep` arg

--- benchmarker/fapp.py
+++ benchmarker/fapp.py
@@ -4,7 +4,7 @@ from benchmarker.fapp_power import get_power, get_total_power
 from benchmarker.util import abstractprocess
 
 
-def run_fapp_profiler(fapp_dir, command):
+def run_fapp_profiler(fapp_dir, rep, command):
     # fapp -C -d ./prof_${APP}_rep${REP} -Hevent=pa${REP} ./${APP}
     fapp_measure_cmd = ["fapp", "-C", "-d"]
     fapp_measure_cmd += [fapp_dir, f"-Hevent=pa{rep}"]
@@ -28,7 +28,7 @@ def get_power_total_and_detail(command):
         for rep in [1, 8]:
             with TemporaryDirectory(suffix=str(rep)) as fapp_dir:
                 csv_file = f"{csv_dir}/pa{rep}.csv"
-                run_fapp_profiler(fapp_dir, command)
+                run_fapp_profiler(fapp_dir, rep, command)
                 gen_fapp_csv(fapp_dir, csv_file)
         power_details = get_power(csv_dir)
         power_total = get_total_power(power_details)
