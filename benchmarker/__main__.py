313e39e2945e6db54ba82900247aae31d5390228
Emil VATAI <emil.vatai@gmail.com>
2020-11-05 21:47:34 +0900

    Move and rename `watt_total`

--- benchmarker/__main__.py
+++ benchmarker/__main__.py
@@ -61,8 +61,8 @@ def main():
         if args.flops:
             result["problem"]["gflop_measured"] = get_counters(command)
         elif args.fapp_power:
-            total, details = fapp.get_power_total_and_detail(command)
-            result["problem"]["power"] = {"total": total, "details": details}
+            watt_total, details = fapp.get_power_total_and_detail(command)
+            result["power"] = {"watt_total": watt_total, "details": details}
     cute_device = get_cute_device_str(result["device"]).replace(" ", "_")
     result["path_out"] = os.path.join(result["path_out"], result["problem"]["name"])
     result["path_out"] = os.path.join(result["path_out"], cute_device)
