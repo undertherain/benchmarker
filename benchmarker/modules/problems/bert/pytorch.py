b65cfb172f22f82e56c6db673b653dc56630638f
Emil VATAI <emil.vatai@gmail.com>
2020-11-05 12:28:24 +0900

    Make it pretty!

--- benchmarker/modules/problems/bert/pytorch.py
+++ benchmarker/modules/problems/bert/pytorch.py
@@ -1,10 +1,24 @@
+from benchmarker.modules.problems.attention.pytorch import \
+    estimate_attention_gflop_per_sample
 from benchmarker.modules.problems.helpers_torch_bert import get_kernel_by_name
-from benchmarker.modules.problems.attention.pytorch import estimate_attention_gflop_per_sample
+
+
+def estimate_gflop_per_sample(len_seq, embed_dim, lin_dim, nb_layers):
+    attention_gflop = estimate_attention_gflop_per_sample(len_seq, embed_dim)
+    mid_linear_gflop = 2 * embed_dim * lin_dim / (10 ** 9)
+    top_linear_gflop = 2 * embed_dim * lin_dim / (10 ** 9)
+    layer_gflop = attention_gflop + mid_linear_gflop + top_linear_gflop
+    return nb_layers * layer_gflop
 
 
 def get_kernel(params):
-    gflop_per_sample = estimate_attention_gflop_per_sample(len_seq, embed_dim)
-    # add dense
-    # * num layers
-    params["problem"]["gflop_estimated"] = gflop_per_sample * cnt_samples * params["nb_epoch"]
+    cnt_samples = params["problem"]["size"][0]
+    gflop_per_sample = estimate_gflop_per_sample(
+        len_seq=params["problem"]["size"][1],
+        embed_dim=params["problem"]["size"][2],
+        lin_dim=3072,
+        nb_layers=12,
+    )
+    gflop_estimated = gflop_per_sample * cnt_samples * params["nb_epoch"]
+    params["problem"]["gflop_estimated"] = gflop_estimated
     return get_kernel_by_name(params, "bert-base-uncased")
