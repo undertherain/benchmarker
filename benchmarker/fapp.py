49d5b144d689a032baf91121231d9d580caf3a15
Emil VATAI <emil.vatai@gmail.com>
2020-11-05 22:20:12 +0900

    Fix function name

--- benchmarker/fapp.py
+++ benchmarker/fapp.py
@@ -4,7 +4,7 @@ from benchmarker.fapp_power import get_power, get_total_power
 from benchmarker.util import abstractprocess
 
 
-def call_fap(cmd):
+def call_fapp(cmd):
     proc = abstractprocess.Process("local", command=cmd)
     output = proc.get_output()
     retval = output["returncode"]
@@ -20,14 +20,14 @@ def run_fapp_profiler(fapp_dir, rep, command):
     fapp_measure_cmd = ["fapp", "-C", "-d"]
     fapp_measure_cmd += [fapp_dir, f"-Hevent=pa{rep}"]
     cmd = fapp_measure_cmd + command
-    call_fap(cmd)
+    call_fapp(cmd)
 
 
 def gen_fapp_csv(fapp_dir, csv_file):
     # fapp -A -tcsv -o $APP_reps/pa$REP.csv -d ./prof_$APP_rep$REP -Icpupa
     fapp_gen_csv_cmd = ["fapp", "-A", "-tcsv"]
     fapp_gen_csv_cmd += ["-o", csv_file] + ["-d", fapp_dir, "-Icpupa"]
-    call_fap(fapp_gen_csv_cmd)
+    call_fapp(fapp_gen_csv_cmd)
 
 
 def get_power_total_and_detail(command):
