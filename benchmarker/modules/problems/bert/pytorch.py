94200e4cf13e11b644656e04339a81ff0a6861da
Sasha <alexander.drozd@gmail.com>
2020-11-05 12:38:30 +0900

    todo layer norm

--- benchmarker/modules/problems/bert/pytorch.py
+++ benchmarker/modules/problems/bert/pytorch.py
@@ -8,6 +8,7 @@ def estimate_gflop_per_sample(len_seq, embed_dim, lin_dim, nb_layers):
     mid_linear_gflop = 2 * embed_dim * lin_dim / (10 ** 9)
     top_linear_gflop = 2 * embed_dim * lin_dim / (10 ** 9)
     layer_gflop = attention_gflop + mid_linear_gflop + top_linear_gflop
+    # TODO: add layer norm
     return nb_layers * layer_gflop
 
 
