22715f181cac960f6607e7751155373b61d55cbf
Shweta Salaria <salaria.shweta@gmail.com>
2020-11-06 09:08:02 +0900

    save profiling logs in profile folder

--- benchmarker/modules/do_pytorch.py
+++ benchmarker/modules/do_pytorch.py
@@ -27,13 +27,10 @@ class Benchmark(INeuralNet):
         parser.add_argument("--no_cudnn_benchmark", dest="cbm", action="store_false")
         parser.add_argument("--precision", default="FP32")
         parser.add_argument("--profile_pytorch", dest="profile", action="store_true")
-        parser.add_argument("--profile_depth", type=int, default=2)
         parser.set_defaults(cbm=True)
         args, remaining_args = parser.parse_known_args(extra_args)
         super().__init__(params, remaining_args)
         self.params["profile_pytorch"] = args.profile
-        if args.profile:
-            self.params["profile_depth"] = args.profile_depth
         self.params["channels_first"] = True
         params["problem"]["precision"] = args.precision
         self.params["backend"] = args.backend
@@ -97,13 +94,13 @@ class Benchmark(INeuralNet):
                     profile_cuda = False
                     if self.device.type == "cuda":
                         profile_cuda = True
-                    with Profile(model, use_cuda=profile_cuda, depth=self.params["profile_depth"]) as prof:
+                    with Profile(model, use_cuda=profile_cuda) as prof:
                         model(data)
                     #print(prof.display(show_events=False))
-                    profile_output_as_json = prof.display()
-                    print(profile_output_as_json)
-                    profile_output = json.loads(profile_output_as_json)
-                    self.params["profile_summary"] = profile_output["ClassifierInference"]
+                    profile_output_as_dict = prof.display(show_events=False)
+                    #print(profile_output_as_dict)
+                    self.params["path_out"] = "./logs/profile/"
+                    self.params["profile_data"] = profile_output_as_dict
 
         if self.params["nb_gpus"] > 0:
             torch.cuda.synchronize()
