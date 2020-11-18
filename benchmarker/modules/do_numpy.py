47df48ffd035eb89c4a622742ee533158f659276
Sasha <alexander.drozd@gmail.com>
2020-11-19 06:50:03 +0900

    don't use pypapi

--- benchmarker/modules/do_numpy.py
+++ benchmarker/modules/do_numpy.py
@@ -5,7 +5,6 @@
 from timeit import default_timer as timer
 import numpy as np
 from .i_gemm import IGEMM
-from pypapi import events, papi_high as high
 
 
 class Benchmark(IGEMM):
@@ -22,19 +21,11 @@ class Benchmark(IGEMM):
         dtype = types[self.params["problem"]["precision"]]
         a = np.random.random((M, N)).astype(dtype)
         b = np.random.random((N, K)).astype(dtype)
-        c = np.random.random((M, K)).astype(dtype)
-        papi_availalbe = True
-        try:
-            high.start_counters([events.PAPI_SP_OPS])
-        except:
-            papi_availalbe = False
+        # c = np.random.random((M, K)).astype(dtype)
         time_start = timer()
         for _ in range(self.params["nb_epoch"]):
             c = a @ b  # + c
         time_end = timer()
-        if papi_availalbe:
-            gflop_papi = high.stop_counters()[0] / (1024 ** 3)
-            self.params["GFLOP_papi"] = gflop_papi
         elapsed_time = (time_end - time_start)
         self.params["time_total"] = elapsed_time
         self.post_process()
