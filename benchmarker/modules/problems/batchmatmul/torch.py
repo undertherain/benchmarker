1e96bdd1f26e26351b93f21d4c93b73bd6ae12cd
Sasha <alexander.drozd@gmail.com>
2020-11-26 10:45:19 +0900

    missing file

---
+++ benchmarker/modules/problems/batchmatmul/torch.py
@@ -0,0 +1,13 @@
+import torch
+
+
+class Net:
+    def __call__(self, data):
+        x, y = data
+        print(y)
+        result = torch.bmm(x, y)
+        return result.size()
+
+
+def get_kernel(params):
+    return Net()
