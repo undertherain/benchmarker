0cfcaec40a955b5be1abe164c6118a8517ea04dc
Sasha <alexander.drozd@gmail.com>
2021-02-11 17:44:19 +0900

    tests passing

--- benchmarker/kernels/images_randomized.py
+++ benchmarker/kernels/images_randomized.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-
+# TODO: move this to data
 def gen_data(params):
     """generates sinthetic dataset"""
     # input size -> cnt_sequences, len_sequence, cnt_dimensions
@@ -9,11 +9,10 @@ def gen_data(params):
     params["problem"]["len_sequence"] = params["problem"]["size"][1]
     cnt_batches = params["problem"]["size"][0] // params["batch_size"]
     shape = (
-        cnt_batches,
         params["problem"]["len_sequence"],
         params["batch_size"],
         params["problem"]["size"][2],
     )
-    X = np.random.random(shape).astype(np.float32)
-    Y = np.ones((cnt_batches))
+    X = [np.random.random(shape).astype(np.float32) for i in range(cnt_batches)]
+    Y = [np.ones((params["batch_size"])) for i in range(cnt_batches)]
     return X, Y
