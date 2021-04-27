e3fa696a168d90e12ea72e72597df2e0f7570891
Emil VATAI <emil.vatai@gmail.com>
2021-04-27 12:41:53 +0900

    Add fugaku script

---
+++ scripts/fugaku/deeplabv3-submit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH=/home/apps/oss/PyTorch-1.7.0/bin:$PATH
+export LD_LIBRARY_PATH=/home/apps/oss/PyTorch-1.7.0/lib:$LD_LIBRARY_PATH
+
+python3 -m benchmarker --framework=pytorch --problem=deeplabv3_resnet50 --problem_size=32
