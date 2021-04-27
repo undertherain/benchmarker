1c70459f00516eaca38fcd3379d8e6a8f97c177f
Emil VATAI <emil.vatai@riken.jp>
2021-04-27 13:36:51 +0900

    Update fugaku deeplab script

--- scripts/fugaku/deeplabv3-submit.sh
+++ scripts/fugaku/deeplabv3-submit.sh
@@ -3,4 +3,4 @@
 export PATH=/home/apps/oss/PyTorch-1.7.0/bin:$PATH
 export LD_LIBRARY_PATH=/home/apps/oss/PyTorch-1.7.0/lib:$LD_LIBRARY_PATH
 
-python3 -m benchmarker --framework=pytorch --problem=deeplabv3_resnet50 --problem_size=32
+python3 -m benchmarker --framework=pytorch --problem=deeplabv3_resnet50 --problem_size=32 --nb_epoch=3 --power_rapl
