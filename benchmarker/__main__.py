d22728898cf607bb9db37ec31b48b2cde5666c1c
Emil Vatai <emil.vatai@gmail.com>
2021-04-30 11:09:58 +0900

    Hide pyrapl (#166)
    * Hide pyrapl
    
    * Hide power_monitor imports
    
    * Add `power_rapl`
    
    * Fix argparse `power_rapl` stuff
    
    * Remove type?!
    
    * Relocate `import pyRAPL`
    
    * Factor out `get_frameword()`
    
    * Rearrange parser args
    
    * Cleanup `get_framework()`
    
    * Refactor `get_problem()` and Refactor `get_gpus()`
    
    * Small cleanup
    
    * Make profiling class names pep 8
    
    * Add fugaku script
    
    * Add descriptions
    
    * Add `--power_nvml` arg
    
    * Update fugaku deeplab script
    
    * Factor out power measurement
    
    * [Big ugly commit] Remove double entry point
    
    * Add todo
    
    Co-authored-by: Emil VATAI <emil.vatai@riken.jp>

--- benchmarker/__main__.py
+++ benchmarker/__main__.py
@@ -5,52 +5,65 @@ import json
 import os
 import sys
 
+import benchmarker.benchmarker
 from benchmarker.profiling import fapp, nvprof, perf
+from benchmarker.results import add_result_details
 # from .benchmarker import run
 from benchmarker.util import abstractprocess
 from benchmarker.util.cute_device import get_cute_device_str
-from benchmarker.results import add_result_details
+
 from .util import sysinfo
 from .util.io import save_json
 
 
-def filter_json_from_output(lines):
-    parts = lines.split("benchmarkmagic#!%")
-    # for p in parts:
-    #     print(p)
-    #     print("\n")
-    str_json = parts[-1].strip()
-    return json.loads(str_json)
-
-def run_cmd_and_get_output(command):
-    proc = abstractprocess.Process("local", command=command)
-    proc_output = proc.get_output()
-    returncode = proc_output["returncode"]
-
-    if returncode != 0:
-        process_err = proc_output["err"]
-        sys.exit(process_err)
-
-    process_out = proc_output["out"]
-    result = filter_json_from_output(process_out)
-    return result
-
-
-def main():
+def get_args():
     parser = argparse.ArgumentParser(description="Benchmark me up, Scotty!")
     parser.add_argument("--flops", action="store_true")
     parser.add_argument("--fapp_power", action="store_true")
-    parser.add_argument("--profile_pytorch", action="store_true")
-    args, unknown_args = parser.parse_known_args()
-    command = [sys.executable, "-m", "benchmarker.benchmarker"]
+    # removed: see issue #167
+    # parser.add_argument("--profile_pytorch", action="store_true")
+    return parser.parse_known_args()
+
+
+# removed: see issue #167
+# def filter_json_from_output(lines):
+#     parts = lines.split("benchmarkmagic#!%")
+#     # for p in parts:
+#     #     print(p)
+#     #     print("\n")
+#     str_json = parts[-1].strip()
+#     return json.loads(str_json)
+
+
+# def run_cmd_and_get_output(command):
+#     proc = abstractprocess.Process("local", command=command)
+#     proc_output = proc.get_output()
+#     returncode = proc_output["returncode"]
+
+#     if returncode != 0:
+#         process_err = proc_output["err"]
+#         sys.exit(process_err)
+
+#     process_out = proc_output["out"]
+#     result = filter_json_from_output(process_out)
+#     return result
+
+
+def main():
+    args, unknown_args = get_args()
+    command = [sys.executable, "-m", "benchmarker"]
     command += unknown_args
-    result = run_cmd_and_get_output(command)
+    # TODO(vatai): remove rapl and nvml and any other profiling. Make
+    # a base class (or something) which ignores this)
+    result = benchmarker.benchmarker.run(unknown_args)
+
     # TODO: don't parse path_out in the innder loop
     result["platform"] = sysinfo.get_sys_info()
     if result["nb_gpus"] > 0:
+        precision = result["problem"]["precision"]
         result["device"] = result["platform"]["gpus"][0]["brand"]
-        if args.flops and result["problem"]["precision"] in ["FP16", "FP32"]:
-            result["problem"]["gflop_measured"] = nvprof.get_gflop(command, result["problem"]["precision"])
+        if args.flops and precision in ["FP16", "FP32"]:
+            result["problem"]["gflop_measured"] = nvprof.get_gflop(command, precision)
     else:
         if (
             "brand" not in result["platform"]["cpu"]
@@ -60,24 +73,28 @@ def main():
             result["device"] = "unknown CPU"
         else:
             result["device"] = result["platform"]["cpu"]["brand"]
-        if args.flops and 'Intel' in result["device"]:
+        if args.flops and "Intel" in result["device"]:
             result["problem"]["gflop_measured"] = perf.get_gflop(command)
         elif args.fapp_power:
             avg_watt_total, details = fapp.get_power_total_and_detail(command)
             result["power"] = {"avg_watt_total": avg_watt_total, "details": details}
+
+    # removed: see issue #167
     # Collect profile data when profile_pytorch switch is enabled
-    if args.profile_pytorch:
-        command += ["--profile_pytorch"]
-        profile_result = run_cmd_and_get_output(command)
-        result["profile_pytorch"] = True
-        result["profile_data"] = profile_result["profile_data"]
-        result["path_out"] = "./logs/profile"
+    # if args.profile_pytorch:
+    #     command += ["--profile_pytorch"]
+    #     profile_result = run_cmd_and_get_output(command)
+    #     result["profile_pytorch"] = True
+    #     result["profile_data"] = profile_result["profile_data"]
+    #     result["path_out"] = "./logs/profile"
 
     cute_device = get_cute_device_str(result["device"]).replace(" ", "_")
     result["path_out"] = os.path.join(result["path_out"], result["problem"]["name"])
     result["path_out"] = os.path.join(result["path_out"], cute_device)
+
     # TODO: call fill_result_details here
     add_result_details(result)
+    print(result)
     save_json(result)
     # TODO: don't measure power when measureing flops
 
--- benchmarker/profiling/power_RAPL.py
+++ benchmarker/profiling/power_RAPL.py
@@ -1,26 +1,23 @@
-import pyRAPL
 import logging
 
+import pyRAPL
 
-class power_monitor_RAPL:
+
+class PowerMonitorRAPL:
     def __init__(self, params):
         self.params = params
         try:
             pyRAPL.setup()
             self.rapl_enabled = True
+            self.meter_rapl = pyRAPL.Measurement("bar")
+            self.meter_rapl.begin()
         except:
             self.rapl_enabled = False
-
-    def start(self):
-        if self.rapl_enabled:
-            self.meter_rapl = pyRAPL.Measurement('bar')
-            self.meter_rapl.begin()
-        else:
-            logger = logging.getLogger(__name__)
-            logger.warning("RAPL requested but it is not enabled")
 
     def stop(self):
         if self.rapl_enabled:
             self.meter_rapl.end()
-            self.params["power"]["joules_CPU"] = sum(self.meter_rapl.result.pkg) / 1000000.0
-            self.params["power"]["joules_RAM"] = sum(self.meter_rapl.result.dram) / 1000000.0
+            joules_CPU = sum(self.meter_rapl.result.pkg) / 1000000.0
+            joules_RAM = sum(self.meter_rapl.result.dram) / 1000000.0
+            self.params["power"]["joules_CPU"] = joules_CPU
+            self.params["power"]["joules_RAM"] = joules_RAM
