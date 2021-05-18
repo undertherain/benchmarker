68b94159dce414e230e5ebeacacf3af9462518ff
Emil VATAI <emil.vatai@gmail.com>
2021-05-18 10:28:45 +0900

    Add `fugaku/bert_ok.sh`

--- scripts/fugaku/resent50_ok.sh
+++ scripts/fugaku/resent50_ok.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-LD_PRELOAD=libtcmalloc.so OMP_NUM_THREADS=12 run_on_cmg python3 -m benchmarker --problem=resnet50 --framework=pytorch --problem_size=120 --batch_size=24 --nb_epoch=3 --mode=training --backend=DNNL --tensor_layout=DNNL
+# sps: 10
+LD_PRELOAD=libtcmalloc.so OMP_NUM_THREADS=12 numactl -N 4 -m 4 python3 -m benchmarker --framework=pytorch --problem=bert --problem_size=120,128 --batch_size=24 --nb_epoch=5 --mode=training --backend=DNNL --tensor_layout=DNNL
