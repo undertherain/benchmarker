15ac5a7a764c8a78a4337d452052723babb9cdd5
Emil VATAI <emil.vatai@gmail.com>
2021-05-27 13:44:22 +0900

    Clean up `abci/bert-ok.sh`

--- scripts/abci/bert_ok.sh
+++ scripts/abci/bert_ok.sh
@@ -2,7 +2,7 @@
 #$ -cwd
 #$ -l rt_F=8
 #$ -l h_rt=07:55:00
-#$ -N benchmarker-deeplabv3
+#$ -N benchmarker-bert-ok
 #$ -j y
 #$ -o $JOB_NAME.o$JOB_ID
 
@@ -11,5 +11,12 @@ source /etc/profile.d/modules.sh
 module load gcc/9.3.0
 module load python/3.8/3.8.7
 # sps: 161
-python3 -m benchmarker --framework=pytorch --problem=bert --problem_size=320,128 --batch_size=64 --nb_epoch=5 --mode=training --gpus=0
+python3 -m benchmarker \
+        --framework=pytorch \
+        --problem=bert \
+        --problem_size=320,128 \
+        --batch_size=64 \
+        --nb_epoch=5 \
+        --mode=training \
+        --gpus=0
 
