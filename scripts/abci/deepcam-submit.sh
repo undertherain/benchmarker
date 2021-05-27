8cb17f69b95aa57f0a177d29bf20e946f49b20a1
Emil VATAI <emil.vatai@gmail.com>
2021-05-27 13:46:01 +0900

    Add `abci/deepcam-submit.sh`

---
+++ scripts/abci/deepcam-submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#$ -cwd
+#$ -l rt_F=1
+#$ -l h_rt=01:00:00
+#$ -N benchmarker-deepcam
+#$ -j y
+#$ -o $JOB_NAME.o$JOB_ID
+
+# run with: qsub -g gcb50300 <this file>
+source /etc/profile.d/modules.sh
+module load gcc/9.3.0
+module load python/3.8/3.8.7
+
+python3 -m benchmarker \
+        --gpus=0 \
+        --framework=pytorch \
+        --problem=deepcam \
+        --problem_size=320 \
+        --batch_size=64
