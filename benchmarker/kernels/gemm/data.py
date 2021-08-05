97e4b00d06abc50fae86332d9922a4f91c819e67
Emil VATAI <emil.vatai@gmail.com>
2021-08-05 23:00:16 +0900

    Blacken

--- benchmarker/kernels/gemm/data.py
+++ benchmarker/kernels/gemm/data.py
@@ -6,13 +6,11 @@ def get_data(params):
     if isinstance(params["problem"]["size"], int):
         params["problem"]["size"] = [params["problem"]["size"]] * 3
     M, N, K = params["problem"]["size"]
-    flop = (2.0 * M * N * K)
+    flop = 2.0 * M * N * K
     params["problem"]["flop_estimated"] = flop * params["nb_epoch"]
     if params["framework"] == "torch":
         M, N, K = params["problem"]["size"]
-        types = {"FP16": np.float16,
-                 "FP32": np.float32,
-                 "FP64": np.float64}
+        types = {"FP16": np.float16, "FP32": np.float32, "FP64": np.float64}
         dtype = types[params["problem"]["precision"]]
         a = torch.tensor(np.random.random((M, N)).astype(dtype))
         b = torch.tensor(np.random.random((N, K)).astype(dtype))
