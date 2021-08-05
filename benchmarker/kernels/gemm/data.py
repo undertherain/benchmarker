5a794d8b4573741f80c4fb575ddf5d7a74ceb217
Emil VATAI <emil.vatai@gmail.com>
2021-08-05 23:00:30 +0900

    Move torch from `data.py` to `torch.py`

--- benchmarker/kernels/gemm/data.py
+++ benchmarker/kernels/gemm/data.py
@@ -1,4 +1,3 @@
-import torch
 import numpy as np
 
 
@@ -12,8 +11,8 @@ def get_data(params):
         M, N, K = params["problem"]["size"]
         types = {"FP16": np.float16, "FP32": np.float32, "FP64": np.float64}
         dtype = types[params["problem"]["precision"]]
-        a = torch.tensor(np.random.random((M, N)).astype(dtype))
-        b = torch.tensor(np.random.random((N, K)).astype(dtype))
-        c = torch.tensor(np.random.random((M, K)).astype(dtype))
+        a = np.random.random((M, N)).astype(dtype)
+        b = np.random.random((N, K)).astype(dtype)
+        c = np.random.random((M, K)).astype(dtype)
         return a, b, c
     return ["dummy data"] * 3
--- benchmarker/kernels/gemm/torch.py
+++ benchmarker/kernels/gemm/torch.py
@@ -1,6 +1,9 @@
+import torch
+
+
 class Kernel:
     def __call__(self, data):
-        x, y, c = data
+        x, y, c = tuple(map(torch.tensor, data))
         c = x @ y  # + c
         return c
 
