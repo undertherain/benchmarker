a6b913355c8c531d1f99837cf4dde46208e902f8
Sasha <alexander.drozd@gmail.com>
2021-09-08 18:35:24 +0900

    dime works but collapses to nans

--- benchmarker/kernels/dimenet/data.py
+++ benchmarker/kernels/dimenet/data.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-cnt_atoms = 100
+cnt_atoms = 10
 
 
 def get_molecule():
-    atoms = np.random.randint(low=1, high=100, size=cnt_atoms, dtype=np.int64)
+    atoms = np.random.randint(low=1, high=60, size=cnt_atoms, dtype=np.int64)
     positions = np.random.random((cnt_atoms, 3)).astype(np.float32)
     return {"z": atoms, "pos": positions}
 
@@ -12,5 +12,5 @@ def get_molecule():
 def get_data(params):
     cnt_batches = 4
     X = [get_molecule() for i in range(cnt_batches)]
-    Y = [np.ones((params["batch_size"])) for i in range(cnt_batches)]
+    Y = [np.ones((params["batch_size"])).astype(np.float32) / 32 for i in range(cnt_batches)]
     return X, Y
--- benchmarker/kernels/dimenet/pytorch.py
+++ benchmarker/kernels/dimenet/pytorch.py
@@ -1,12 +1,25 @@
+import torch
+# from benchmarker.kernels.helpers_torch import Regression
 from torch_geometric.nn import DimeNet
+
+
+class Wrapper(torch.nn.Module):
+    def __init__(self, net):
+        super().__init__()
+        self.net = net
+
+    def __call__(self, x, y):
+        res = self.net(**x)
+        loss = torch.nn.functional.mse_loss(res.sum(), y)
+        return loss
 
 
 def get_kernel(params):
     net = DimeNet(
-        hidden_channels=128,
-        out_channels=128,
+        hidden_channels=16,
+        out_channels=16,
         num_blocks=3,
-        num_bilinear=8,
-        num_spherical=7,
-        num_radial=6)
-    return net
+        num_bilinear=2,
+        num_spherical=4,
+        num_radial=4)
+    return Wrapper(net)
