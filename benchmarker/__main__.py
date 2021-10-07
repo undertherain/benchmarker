85367a3167b52c84961508654dd0305c60f2d7af
Emil VATAI <emil.vatai@gmail.com>
2021-10-07 11:09:32 +0900

    Move stuff to a single function

--- benchmarker/__main__.py
+++ benchmarker/__main__.py
@@ -11,6 +11,23 @@ from .util import sysinfo
 from .util.io import get_time_str, save_json
 
 
+def fixup_for_amd_gpus(result):
+    same_gpu_count = len(result["platform"]["gpus"]) == result["nb_gpus"]
+    using_pytorch = result["framework"] == "pytorch"
+    if using_pytorch and not same_gpu_count:
+        import torch.cuda
+
+        nb_gpus = torch.cuda.device_count()
+        gpus = []
+        for i in range(nb_gpus):
+            device_name = torch.cuda.get_device_name(i)
+            assert device_name == "Device 738c"
+            gpu = {"brand": "AMD", "name": "Mi100"}
+            gpus.append(gpu)
+
+        result["platform"]["gpus"] = gpus
+
+
 def get_args():
     parser = argparse.ArgumentParser(description="Benchmark me up, Scotty!")
     parser.add_argument("--flops", action="store_true", default=False)
@@ -54,23 +71,7 @@ def main():
 
     # TODO: don't parse path_out in the innder loop
     result["platform"] = sysinfo.get_sys_info()
-
-    ## start
-    same_gpu_count = len(result["platform"]["gpus"]) == result["nb_gpus"]
-    using_pytorch = result["framework"] == "pytorch"
-    if using_pytorch and not same_gpu_count:
-        import torch.cuda
-
-        nb_gpus = torch.cuda.device_count()
-        gpus = []
-        for i in range(nb_gpus):
-            gpu = {"brand": "AMD", "name": torch.cuda.get_device_name(i)}
-            gpus.append(gpu)
-
-        print("RESULT GPUS", result["platform"]["gpus"])
-        print("GPUS", gpus)
-        result["platform"]["gpus"] = gpus
-    ## end
+    fixup_for_amd_gpus(result)
 
     result["start_time"] = get_time_str()
     if result["nb_gpus"] > 0:
