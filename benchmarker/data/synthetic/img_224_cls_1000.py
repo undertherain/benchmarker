8e104d7e2e654a0918bc9dbb12a92ec6729f1189
Aleksandr Drozd <alexander.drozd@gmail.com>
2023-03-25 11:24:15 +0900

    fix imaganet data helper

--- benchmarker/data/synthetic/img_224_cls_1000.py
+++ benchmarker/data/synthetic/img_224_cls_1000.py
@@ -4,10 +4,10 @@
 from .helpers import gen_classification_data
 
 
-def get_data(params):
+def get_data(params, name_key="x"):
     """Generate synthetic 224x224 images. Set `params["size"]`
     appropriately. Import this function in the `data.py` of the
     problem, so it can be called by `INeuralNet`.
 
     """
-    return gen_classification_data(params, 1000, 224, name_key)
+    return gen_classification_data(params, 1000, 224, name_key=name_key)
