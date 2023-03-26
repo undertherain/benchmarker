7ce715aec7c6ae3f434fa0f3f9aef61bb3856c6e
Aleksandr Drozd <alexander.drozd@gmail.com>
2023-03-26 14:03:35 +0900

    reuse lm data gen

--- benchmarker/kernels/roberta_mlm/data.py
+++ benchmarker/kernels/roberta_mlm/data.py
@@ -1,16 +1,1 @@
-import numpy as np
-
-
-def get_data(params):
-    if isinstance(params["problem"]["size"], int):
-        params["problem"]["size"] = (params["problem"]["size"], 128)
-    assert params["problem"]["size"][0] % params["batch_size"] == 0
-    params["problem"]["len_sequence"] = params["problem"]["size"][1]
-    cnt_batches = params["problem"]["size"][0] // params["batch_size"]
-    shape = (params["batch_size"],
-             params["problem"]["len_sequence"],
-             )
-    # TODO: use amx token id here
-    X = np.random.randint(low=0, high=1024, size=shape).astype(np.int64)
-    res = [{"input_ids": X, "labels": X} for i in range(cnt_batches)]
-    return res
+from benchmarker.data.synthetic.language_modeling import get_data
