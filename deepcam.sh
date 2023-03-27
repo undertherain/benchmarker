5e04f6d0d3ae01aa6fc7a499baa51fe5da4c5236
Aleksandr Drozd <alexander.drozd@gmail.com>
2023-03-27 15:55:38 +0900

    deepcam

--- deepcam.sh
+++ deepcam.sh
@@ -1,8 +1,9 @@
 python3 -m benchmarker \
     --framework=pytorch \
     --problem=deepcam \
-    --problem_size=60 \
+    --problem_size=60,3,512,512 \
     --batch_size=6 \
+    --preheat \
     --mode=inference \
     --gpus=0 \
-    --nb_epoch=1
+    --nb_epoch=2
