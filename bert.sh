1b1cfa26334239df03a3da4f7eee0a084a898a00
Aleksandr Drozd <alexander.drozd@gmail.com>
2023-03-27 14:21:52 +0900

    tf32

--- bert.sh
+++ bert.sh
@@ -1,12 +1,14 @@
 # CPU:
 
-python3 -m benchmarker.benchmarker \
+python3 -m benchmarker \
     --framework=pytorch \
     --problem=roberta_large_mlm \
-    --problem_size=64,256 \
+    --problem_size=512,256 \
     --batch_size=32 \
-    --nb_epoch=1 \
+    --nb_epoch=2 \
     --gpus=0 \
-    --mode=inference
+    --preheat \
+    --mode=inference \
+    --precision=TF32 \
 
 #    --flops \
