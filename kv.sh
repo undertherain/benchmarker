c7e749073fac3d55ddc791573db5d4550e3b6fe3
Aleksandr Drozd <alexander.drozd@gmail.com>
2025-03-19 12:24:22 +0900

    kv sh

---
+++ kv.sh
@@ -0,0 +1,16 @@
+LD_PRELOAD=libtcmalloc.so \
+python3 -m benchmarker.benchmarker \
+    --framework=pytorch \
+    --problem=kv \
+    --embedding_size=8000    \
+    --nb_epoch=10 \
+    --numerics=FP16 \
+    --precision=highest \
+    --cnt_samples=32 \
+    --batch_size=1 \
+    --preheat \
+    # --gpus=0 \
+#    --batch_size=1536 \
+#    --enable_TF32    
+
+
